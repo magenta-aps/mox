86a4d48c0e664b04c6407e2cf75a6e84dad6d5c3
Seth Yastrov <seth@magenta.dk>
2015-01-29 10:23:01 +0100

    Updated example to be more complex (more attributes).

--- actualstate/sql/examples.sql
+++ actualstate/sql/examples.sql
@@ -6,16 +6,32 @@ BEGIN
   -- Example call to create a user
   SELECT ID FROM ACTUAL_STATE_CREATE_BRUGER(
     ARRAY[
-    -- BrugerEgenskaberType
       ROW (
-    --   Virkning
         ROW (
-          tstzrange(now(), 'infinity', '[]'),
+          tstzrange('2015-01-01', '2015-01-10', '[)'),
           uuid_generate_v4(),
           'Bruger',
           'Note'
         )::Virkning,
         'BrugervendtNoegle', 'Brugernavn', 'Brugertype'
+      )::BrugerEgenskaberType,
+      ROW (
+        ROW (
+          tstzrange('2015-01-10', '2015-01-20', '[)'),
+          uuid_generate_v4(),
+          'Bruger',
+          'Note2'
+        )::Virkning,
+        'BrugervendtNoegle2', 'Brugernavn2', 'Brugertype2'
+      )::BrugerEgenskaberType,
+      ROW (
+        ROW (
+          tstzrange('2015-01-20', 'infinity', '(]'),
+          uuid_generate_v4(),
+          'Bruger',
+          'Note3'
+        )::Virkning,
+        'BrugervendtNoegle3', 'Brugernavn3', 'Brugertype3'
       )::BrugerEgenskaberType
     ],
     ARRAY[
@@ -36,7 +52,8 @@ BEGIN
 
   PERFORM actual_state_read_bruger(brugerId,
                                          -- Virkning period
-                                         TSTZRANGE('2015-01-20', 'infinity', '[]'),
+                                         TSTZRANGE('2015-01-01',
+                                                   '2015-01-15', '[]'),
                                          -- Registrering period
                                          TSTZRANGE(now(), 'infinity', '[]'),
                                          -- Cursor name
