686306482bfa408374b0c9cd0a0d452e751ae6a0
Seth Yastrov <seth@magenta.dk>
2015-02-02 12:03:13 +0100

    Add ACTUAL_STATE_BRUGER_UPDATE.

--- actualstate/sql/crud.sql
+++ actualstate/sql/crud.sql
@@ -45,7 +45,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
 CREATE OR REPLACE FUNCTION ACTUAL_STATE_READ_BRUGER(
     ID UUID,
     VirkningPeriod TSTZRANGE,
@@ -80,5 +79,68 @@ BEGIN
   WHERE brugerregistreringid = result.id AND
         (virkning).TimePeriod && VirkningPeriod;
   RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION ACTUAL_STATE_NEW_REGISTRATION_BRUGER(
+  inputID UUID,
+  LivscyklusKode LivscyklusKode,
+  BrugerRef UUID
+) RETURNS BIGINT AS $$
+DECLARE
+  registreringTime TIMESTAMPTZ := now();
+  brugerRegistreringID BIGINT;
+BEGIN
+-- Update previous Registrering's time range to end now, exclusive
+  UPDATE BrugerRegistrering SET Registrering.TimePeriod = TSTZRANGE(
+      lower((Registrering).TimePeriod), registreringTime, '[)')
+  WHERE BrugerID = inputID;
+--   Create Registrering starting from now until infinity
+  INSERT INTO BrugerRegistrering (BrugerID, Registrering) VALUES (
+    inputID,
+    ROW(TSTZRANGE(registreringTime, 'infinity', '[]'), LivscyklusKode, BrugerRef)
+  ) RETURNING ID INTO brugerRegistreringID;
+  RETURN brugerRegistreringID;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION ACTUAL_STATE_UPDATE_BRUGER(
+  inputID UUID,
+  Attributter BrugerEgenskaberType[],
+  Tilstande BrugerTilstandType[]
+)
+  RETURNS BrugerRegistrering AS $$
+DECLARE
+  brugerRegistreringID BIGINT;
+  result BrugerRegistrering;
+BEGIN
+  brugerRegistreringID := ACTUAL_STATE_NEW_REGISTRATION_BRUGER(
+      inputID, 'Rettet', NULL
+  );
+--   Loop through attributes and add them to the registration
+  DECLARE
+    attr BrugerEgenskaberType;
+  BEGIN
+    FOREACH attr in ARRAY Attributter
+    LOOP
+      INSERT INTO BrugerEgenskaber (BrugerRegistreringID, Virkning, BrugervendtNoegle, Brugernavn, Brugertype)
+      VALUES (brugerRegistreringID, attr.Virkning, attr.BrugervendtNoegle,
+              attr.Brugernavn, attr.Brugertype);
+    END LOOP;
+  END;
+
+--   Loop through states and add them to the registration
+  DECLARE
+    state BrugerTilstandType;
+  BEGIN
+    FOREACH state in ARRAY Tilstande
+    LOOP
+      INSERT INTO BrugerTilstand (BrugerRegistreringID, Virkning, Status)
+      VALUES (brugerRegistreringID, state.Virkning, state.Status);
+    END LOOP;
+  END;
+
+  SELECT * FROM BrugerRegistrering WHERE ID = brugerRegistreringID INTO result;
+  RETURN result;
 END;
 $$ LANGUAGE plpgsql;
--- actualstate/sql/examples.sql
+++ actualstate/sql/examples.sql
@@ -62,6 +62,68 @@ BEGIN
     ]
   ) INTO brugerId;
 
+
+
+  -- Example call to create a user
+  PERFORM ACTUAL_STATE_UPDATE_BRUGER(
+    brugerId,
+    ARRAY[
+      ROW (
+        ROW (
+          '[2015-01-01, 2015-01-10)'::TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+        )::Virkning,
+        'BrugervendtNoegle', 'Brugernavn', 'Brugertype'
+      )::BrugerEgenskaberType,
+      ROW (
+        ROW (
+          '[2015-01-10, 2015-01-20)'::TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note2'
+        )::Virkning,
+        'BrugervendtNoegle2Updated', 'Brugernavn2Updated', 'Brugertype2Updated'
+      )::BrugerEgenskaberType,
+      ROW (
+        ROW (
+          '[2015-01-20, infinity)'::TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note3Updated'
+        )::Virkning,
+        'BrugervendtNoegle3Updated', 'Brugernavn3Updated', 'Brugertype3Updated'
+      )::BrugerEgenskaberType
+    ],
+    ARRAY[
+      ROW (
+        ROW ('[2015-01-01, 2015-01-10)'::TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+        )::Virkning,
+        'Aktiv'
+      )::BrugerTilstandType,
+      ROW (
+        ROW ('[2015-01-10, 2015-01-20)'::TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+        )::Virkning,
+        'Inaktiv'
+      )::BrugerTilstandType,
+      ROW (
+        ROW ('[2015-01-20, infinity)'::TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+        )::Virkning,
+        'Aktiv'
+      )::BrugerTilstandType
+    ]
+  );
+
   PERFORM actual_state_read_bruger(
       brugerId,
       -- Virkning period
@@ -75,7 +137,7 @@ BEGIN
 END
 $$;
 
-FETCH ALL IN "attributesCursor";
-FETCH ALL IN "statesCursor";
+FETCH ALL FROM "attributesCursor";
+FETCH ALL FROM "statesCursor";
 
 COMMIT;
