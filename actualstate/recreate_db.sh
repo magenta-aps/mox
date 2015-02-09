97c24404bb531b230a3634cf074b0e054711c1d1
Seth Yastrov <seth@magenta.dk>
2015-02-09 16:50:50 +0100

    Add triggers.sql to recreate_db.sh script.

--- actualstate/recreate_db.sh
+++ actualstate/recreate_db.sh
@@ -4,4 +4,5 @@ sudo -u postgres createdb mox
 sudo -u postgres psql -d mox -f sql/common.sql
 sudo -u postgres psql -d mox -f sql/base_tables.sql
 python generate_tables.py | sudo -u postgres psql -d mox
+sudo -u postgres psql -d mox -f sql/triggers.sql
 sudo -u postgres psql -d mox -f sql/crud.sql
