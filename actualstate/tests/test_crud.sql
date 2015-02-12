f040b46242918553c0d681dc39945e78768dc2ec
Seth Yastrov <seth@magenta.dk>
2015-02-12 15:29:34 +0100

    Cleanup tests.

--- actualstate/tests/test_crud.sql
+++ actualstate/tests/test_crud.sql
@@ -284,86 +284,82 @@ BEGIN
         )::TilstandsType
       ]
     ) INTO reg;
-    DECLARE
-      result TEXT;
-    BEGIN
+
 
 --       Test egenskaber
-      RETURN NEXT (SELECT is (e1.Value, 'Brugernavnupdated',
-                              'New property value inserted') FROM Egenskab e1
-        JOIN Egenskaber e2 ON e1.EgenskaberID = e2.ID
-      WHERE e2.RegistreringsID = reg.ID
-            AND (e2.Virkning).TimePeriod = '[2015-01-01, 2015-01-10)'::TSTZRANGE
-            AND e1.Name = 'Brugernavn');
 
-      RETURN NEXT (SELECT is (e1.Value, 'BrugernavnAupdated',
-                              'New property value inserted') FROM Egenskab e1
-        JOIN Egenskaber e2 ON e1.EgenskaberID = e2.ID
-      WHERE e2.RegistreringsID = reg.ID
-            AND (e2.Virkning).TimePeriod = '[2015-01-15, 2015-02-05)'::TSTZRANGE
-            AND e1.Name = 'Brugernavn');
+    RETURN NEXT (SELECT is (e1.Value, 'Brugernavnupdated',
+                            'New property value inserted') FROM Egenskab e1
+      JOIN Egenskaber e2 ON e1.EgenskaberID = e2.ID
+    WHERE e2.RegistreringsID = reg.ID
+          AND (e2.Virkning).TimePeriod = '[2015-01-01, 2015-01-10)'::TSTZRANGE
+          AND e1.Name = 'Brugernavn');
 
-      RETURN NEXT (SELECT is (e1.Value, 'BrugernavnA',
-                              'Old property value''s range is altered') FROM Egenskab e1
-        JOIN Egenskaber e2 ON e1.EgenskaberID = e2.ID
-      WHERE e2.RegistreringsID = reg.ID
-            AND (e2.Virkning).TimePeriod = '[2015-02-05, 2015-02-10)'::TSTZRANGE
-            AND e1.Name = 'Brugernavn');
+    RETURN NEXT (SELECT is (e1.Value, 'BrugernavnAupdated',
+                            'New property value inserted 2') FROM Egenskab e1
+      JOIN Egenskaber e2 ON e1.EgenskaberID = e2.ID
+    WHERE e2.RegistreringsID = reg.ID
+          AND (e2.Virkning).TimePeriod = '[2015-01-15, 2015-02-05)'::TSTZRANGE
+          AND e1.Name = 'Brugernavn');
 
-      RETURN NEXT ok ((SELECT COUNT(e1.*) = 0 FROM Egenskab e1
-                     JOIN Egenskaber e2 ON e1.EgenskaberID = e2.ID
-                   WHERE e2.RegistreringsID = reg.ID
-                         AND e1.Name = 'Brugernavn'
-                         AND e1.Value = 'Brugernavn'), 'Old property value is deleted');
+    RETURN NEXT (SELECT is (e1.Value, 'BrugernavnA',
+                            'Old property value''s range is altered') FROM Egenskab e1
+      JOIN Egenskaber e2 ON e1.EgenskaberID = e2.ID
+    WHERE e2.RegistreringsID = reg.ID
+          AND (e2.Virkning).TimePeriod = '[2015-02-05, 2015-02-10)'::TSTZRANGE
+          AND e1.Name = 'Brugernavn');
+
+    RETURN NEXT ok ((SELECT COUNT(e1.*) = 0 FROM Egenskab e1
+                   JOIN Egenskaber e2 ON e1.EgenskaberID = e2.ID
+                 WHERE e2.RegistreringsID = reg.ID
+                       AND e1.Name = 'Brugernavn'
+                       AND e1.Value = 'Brugernavn'), 'Old property value is deleted');
 
 --       Test tilstand
 
-      RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
-        WHERE RegistreringsID = reg.ID
-              AND (Virkning).TimePeriod = '[2014-11-01, 2014-12-20)'::TSTZRANGE
-                     AND (Virkning).Notetekst = 'Note2'),
-                     'New tilstand completely contains and replaces old');
-      RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
-        WHERE RegistreringsID = reg.ID
-              AND (Virkning).TimePeriod = '[2015-01-01, 2015-01-03)'::TSTZRANGE
-                     AND (Virkning).Notetekst = 'Note'),
-                     'Split old tilstand lower');
-      RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
-        WHERE RegistreringsID = reg.ID
-              AND (Virkning).TimePeriod = '[2015-01-03, 2015-01-05)'::TSTZRANGE
-                     AND (Virkning).Notetekst = 'Note2'),
-                     'New tilstand inserted in middle');
-      RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
-        WHERE RegistreringsID = reg.ID
-              AND (Virkning).TimePeriod = '[2015-01-05, 2015-01-10)'::TSTZRANGE
-                     AND (Virkning).Notetekst = 'Note'),
-                     'Split old tilstand upper');
-      RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
-        WHERE RegistreringsID = reg.ID
-              AND (Virkning).TimePeriod = '[2015-01-10, 2015-01-20)'::TSTZRANGE
-                     AND Status = 'Inaktiv'),
-                     'New tilstand replaced old tilstand with exact same
-                     range');
-      RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
+    RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
       WHERE RegistreringsID = reg.ID
-            AND (Virkning).TimePeriod = '[2015-01-20, 2015-01-25)'::TSTZRANGE
-                     AND (Virkning).Notetekst = 'Note'),
-                     'Old upper bound changed when new overlaps to the right');
-      RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
+            AND (Virkning).TimePeriod = '[2014-11-01, 2014-12-20)'::TSTZRANGE
+                   AND (Virkning).Notetekst = 'Note2'),
+                   'New tilstand completely contains and replaces old');
+    RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
       WHERE RegistreringsID = reg.ID
-            AND (Virkning).TimePeriod = '[2015-01-25, 2015-02-10)'::TSTZRANGE
-                     AND (Virkning).Notetekst = 'Note2'),
-                     'New tilstand inserted between two overlapping old');
-      RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
+            AND (Virkning).TimePeriod = '[2015-01-01, 2015-01-03)'::TSTZRANGE
+                   AND (Virkning).Notetekst = 'Note'),
+                   'Split old tilstand lower');
+    RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
       WHERE RegistreringsID = reg.ID
-            AND (Virkning).TimePeriod = '[2015-02-10, infinity)'::TSTZRANGE
-                     AND (Virkning).Notetekst = 'Note'),
-                     'Old lower bound changed when new overlaps to the left');
-      RETURN NEXT ok((SELECT COUNT(*) = 0 FROM Tilstand
-      WHERE (Virkning).TimePeriod = 'empty'),
-                     'There should be no empty Virkning TimePeriods');
-    END;
---     Check that the properties got replaced
+            AND (Virkning).TimePeriod = '[2015-01-03, 2015-01-05)'::TSTZRANGE
+                   AND (Virkning).Notetekst = 'Note2'),
+                   'New tilstand inserted in middle');
+    RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
+      WHERE RegistreringsID = reg.ID
+            AND (Virkning).TimePeriod = '[2015-01-05, 2015-01-10)'::TSTZRANGE
+                   AND (Virkning).Notetekst = 'Note'),
+                   'Split old tilstand upper');
+    RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
+      WHERE RegistreringsID = reg.ID
+            AND (Virkning).TimePeriod = '[2015-01-10, 2015-01-20)'::TSTZRANGE
+                   AND Status = 'Inaktiv'),
+                   'New tilstand replaced old tilstand with exact same range');
+    RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
+    WHERE RegistreringsID = reg.ID
+          AND (Virkning).TimePeriod = '[2015-01-20, 2015-01-25)'::TSTZRANGE
+                   AND (Virkning).Notetekst = 'Note'),
+                   'Old upper bound changed when new overlaps to the right');
+    RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
+    WHERE RegistreringsID = reg.ID
+          AND (Virkning).TimePeriod = '[2015-01-25, 2015-02-10)'::TSTZRANGE
+                   AND (Virkning).Notetekst = 'Note2'),
+                   'New tilstand inserted between two overlapping old');
+    RETURN NEXT ok((SELECT COUNT(*) = 1 FROM Tilstand
+    WHERE RegistreringsID = reg.ID
+          AND (Virkning).TimePeriod = '[2015-02-10, infinity)'::TSTZRANGE
+                   AND (Virkning).Notetekst = 'Note'),
+                   'Old lower bound changed when new overlaps to the left');
+    RETURN NEXT ok((SELECT COUNT(*) = 0 FROM Tilstand
+    WHERE (Virkning).TimePeriod = 'empty'),
+                   'There should be no empty Virkning TimePeriods');
   END;
 END;
 $$ LANGUAGE plpgsql;
