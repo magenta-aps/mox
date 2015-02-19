c072f8a94015d04efeb977aea5144b09befdc239
Seth Yastrov <seth@magenta.dk>
2015-02-19 14:26:43 +0100

    Added working examples.sql.

--- actualstate/sql/examples.sql
+++ actualstate/sql/examples.sql
@@ -1,208 +1,192 @@
--- Example call to create a user
-SELECT ID FROM ACTUAL_STATE_CREATE(
-  'Bruger',
-  ARRAY[
-    ROW (
-      ARRAY[
-        ROW(
-          'Brugernavn',
-          'Brugernavn'
-        )::EgenskabsType,
-        ROW(
-          'Brugertype',
-          'Brugertype'
-        )::EgenskabsType
-      ],
-      ROW (
-        '[2015-01-01, 2015-01-10)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note'
-      )::Virkning,
-      'BrugervendtNoegle'
-    )::EgenskaberType,
-    ROW (
-      ARRAY[
-        ROW(
-          'Brugernavn',
-          'Brugernavn2'
-        )::EgenskabsType,
-        ROW(
-          'Brugertype',
-          'Brugertype2'
-        )::EgenskabsType
-      ],
-      ROW (
-        '[2015-01-10, 2015-01-20)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note2'
-      )::Virkning,
-     'BrugervendtNoegle2'
-    )::EgenskaberType,
-    ROW (
-      ARRAY[
-        ROW(
-          'Brugernavn',
-          'Brugernavn3'
-        )::EgenskabsType,
-        ROW(
-          'Brugertype',
-          'Brugertype3'
-        )::EgenskabsType
-      ],
-      ROW (
-        '[2015-01-20, infinity)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note3'
-      )::Virkning,
-      'BrugervendtNoegle3'
-    )::EgenskaberType
-  ],
-  ARRAY[
-    ROW (
-      ROW ('[2015-01-01, 2015-01-10)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note'
-      )::Virkning,
-      'Aktiv'
-    )::TilstandsType,
-    ROW (
-      ROW ('[2015-01-10, 2015-01-20)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note'
-      )::Virkning,
-      'Aktiv'
-    )::TilstandsType,
-    ROW (
-      ROW ('[2015-01-20, infinity)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note'
-      )::Virkning,
-      'Aktiv'
-    )::TilstandsType
-  ]
-);
-
-  -- Example call to update a user.
-  -- Assumes there is only one user in the system
-SELECT ACTUAL_STATE_UPDATE(
-    (SELECT ID FROM Bruger LIMIT 1),
-  ARRAY[
-    ROW (
-      ARRAY[
-        ROW(
-          'Brugernavn',
-          'Brugernavn'
-        )::EgenskabsType,
-        ROW(
-          'Brugertype',
-          'Brugertype'
-        )::EgenskabsType
-      ],
-      ROW (
-        '[2015-01-01, 2015-01-10)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note'
-      )::Virkning,
-      'BrugervendtNoegle'
-    )::EgenskaberType,
-    ROW (
-      ARRAY[
-        ROW(
-          'Brugernavn',
-          'Brugernavn2changed'
-        )::EgenskabsType,
-        ROW(
-          'Brugertype',
-          'Brugertype2changed'
-        )::EgenskabsType
-      ],
-      ROW (
-        '[2015-01-10, 2015-01-20)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note2'
-      )::Virkning,
-     'BrugervendtNoegle2'
-    )::EgenskaberType,
-    ROW (
-      ARRAY[
-        ROW(
-          'Brugernavn',
-          'Brugernavn3changed'
-        )::EgenskabsType,
-        ROW(
-          'Brugertype',
-          'Brugertype3changed'
-        )::EgenskabsType
-      ],
-      ROW (
-        '[2015-01-20, infinity)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note3'
-      )::Virkning,
-      'BrugervendtNoegle3changed'
-    )::EgenskaberType
-  ],
-  ARRAY[
-    ROW (
-      ROW ('[2015-01-01, 2015-01-10)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note'
-      )::Virkning,
-      'Aktiv'
-    )::TilstandsType,
-    ROW (
-      ROW ('[2015-01-10, 2015-01-20)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note'
-      )::Virkning,
-      'Inaktiv'
-    )::TilstandsType,
-    ROW (
-      ROW ('[2015-01-20, infinity)'::TSTZRANGE,
-        uuid_generate_v4(),
-        'Bruger',
-        'Note'
-      )::Virkning,
-      'Aktiv'
-    )::TilstandsType
-  ]
-);
-
--- Reading from cursors must be inside a transaction
-BEGIN;
-  SELECT ACTUAL_STATE_READ(
+SELECT * FROM ACTUAL_STATE_CREATE(
       'Bruger',
-      (SELECT ID FROM Bruger LIMIT 1),
-      -- Virkning period
-      '[2015-01-01, 2015-01-15]'::TSTZRANGE,
-      -- Registrering period
-      TSTZRANGE(now(), 'infinity', '[]'),
-      -- Cursor name
-      'attributesCursor',
-      'statesCursor'
+      ARRAY [
+        ROW (
+        'Egenskab',
+        ARRAY [
+          ROW (
+          ARRAY [
+            ROW ('BrugervendtNoegle', 'BrugervendtNoegle'),
+            ROW ('Brugernavn', 'Brugernavn'),
+            ROW ('Brugertype', 'Brugertype')
+          ] :: AttributFeltType [],
+          ROW ('[2015-01-01, 2015-01-10)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning
+        ) :: AttributType,
+          ROW (
+          ARRAY [
+            ROW ('BrugervendtNoegle', 'BrugervendtNoegleA'),
+            ROW ('Brugernavn', 'BrugernavnA'),
+            ROW ('Brugertype', 'BrugertypeA')
+          ] :: AttributFeltType [],
+          ROW ('[2015-02-01, 2015-02-10)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning
+        ) :: AttributType
+        ]
+        ) :: AttributterType
+      ],
+      ARRAY [
+        ROW (
+        'Gyldighed',
+        ARRAY [
+          ROW (
+          ROW ('[2014-12-01, 2014-12-15)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning,
+          'Aktiv'
+        ),
+          ROW (
+          ROW ('[2015-01-01, 2015-01-10)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning,
+          'Aktiv'
+        ),
+          ROW (
+          ROW ('[2015-01-10, 2015-01-20)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning,
+          'Aktiv'
+        ),
+          ROW (
+          ROW ('[2015-01-20, 2015-01-30)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning,
+          'Aktiv'
+        ),
+          ROW (
+          ROW ('[2015-01-30, infinity)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning,
+          'Aktiv'
+        )
+        ] :: TilstandType []
+      ) :: TilstandeType
+      ],
+
+      ARRAY [
+        ROW (
+        'Adresser',
+        ARRAY [
+          ROW (
+          ROW ('[2015-01-20, infinity)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning,
+          ARRAY[uuid_generate_v4()]
+        ) :: RelationType
+        ]
+      ) :: RelationerType
+      ]
   );
+      -- Example call to update a user.
+  SELECT * FROM ACTUAL_STATE_UPDATE(
+      (SELECT ID FROM Bruger LIMIT 1),
+      ARRAY [
+        ROW (
+        'Egenskab',
+        ARRAY [
+--             ROW (
+--             ARRAY [
+--               ROW ('BrugervendtNoegle', 'BrugervendtNoegleupdated'),
+--               ROW ('Brugernavn', 'Brugernavnupdated'),
+--               ROW ('Brugertype', 'Brugertypeupdated')
+--             ] :: AttributFeltType [],
+--             ROW ('[2015-01-01, 2015-01-02)' :: TSTZRANGE,
+--             uuid_generate_v4(),
+--             'Bruger',
+--             'Note'
+--             ) :: Virkning
+--           ) :: AttributType,
+          ROW (
+          ARRAY [
+            ROW ('BrugervendtNoegle', 'BrugervendtNoegleAupdated'),
+            ROW ('Brugernavn', 'BrugernavnAupdated'),
+            ROW ('Brugertype', 'BrugertypeAupdated')
+          ] :: AttributFeltType [],
+          ROW ('[2015-01-06, 2015-01-15)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning
+        ) :: AttributType
+        ]
+      ) :: AttributterType
+      ],
+      ARRAY [
+        ROW (
+        'Gyldighed',
+        ARRAY [
+          ROW (
+          ROW ('[2014-11-01, 2014-12-20)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note2'
+          ) :: Virkning,
+          'Aktiv'
+        ),
+          ROW (
+          ROW ('[2015-01-03, 2015-01-05)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note2'
+          ) :: Virkning,
+          'Aktiv'
+        ),
+          ROW (
+          ROW ('[2015-01-10, 2015-01-20)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning,
+          'Inaktiv'
+        ),
+          ROW (
+          ROW ('[2015-01-25, 2015-02-10)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note2'
+          ) :: Virkning,
+          'Aktiv'
+        )
+        ] :: TilstandType []
+      ) :: TilstandeType
+      ],
 
-  FETCH ALL FROM "attributesCursor";
-  FETCH ALL FROM "statesCursor";
-COMMIT;
+      ARRAY [
+        ROW (
+        'Adresser',
+        ARRAY [
+          ROW (
+          ROW ('[2015-01-20, infinity)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Note'
+          ) :: Virkning,
+          ARRAY[uuid_generate_v4()]
+        ) :: RelationType
+        ]
+      ) :: RelationerType
+      ]
+);
 
-SELECT * FROM BrugerRegistrering;
 
 SELECT ACTUAL_STATE_DELETE('Bruger', (SELECT ID FROM Bruger LIMIT 1));
 
 SELECT ACTUAL_STATE_PASSIVE('Bruger', (SELECT ID FROM Bruger LIMIT 1));
-
-SELECT * FROM BrugerEgenskaber;
-
-SELECT * FROM BrugerTilstand;
