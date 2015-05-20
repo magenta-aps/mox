ad93bc0e0be08d0fb98bd422623401196cc5106a
Thomas Kristensen <thomas@magenta.dk>
2015-05-20 10:33:44 +0200

    Impl. support for multiple attributes in template dbtyper-specific.jinja.sql

--- klassifikation/db/db-templating/templates/dbtyper-specific.jinja.sql
+++ klassifikation/db/db-templating/templates/dbtyper-specific.jinja.sql
@@ -2,19 +2,21 @@
 {% block body %}
 --create custom type sans db-ids to be able to do "clean" function signatures "for the outside world".
 
-{% for key, value in tilstande.iteritems() %}CREATE TYPE {{oio_type|title}}Tils{{key|title}} AS ENUM ({% for enum_val in value %}'{{enum_val}}',{% endfor %}''); --'' means undefined (which is needed to clear previous defined value in an already registered virksnings-periode)
+{% for tilstand, tilstand_values in tilstande.iteritems() %}CREATE TYPE {{oio_type|title}}Tils{{tilstand|title}} AS ENUM ({% for enum_val in tilstand_values %}'{{enum_val}}',{% endfor %}''); --'' means undefined (which is needed to clear previous defined tilstand_values in an already registered virksnings-periode)
 
-CREATE TYPE {{oio_type|title}}Tils{{key|title}}Type AS (
+CREATE TYPE {{oio_type|title}}Tils{{tilstand|title}}Type AS (
     virkning Virkning,
-    {{key}} {{oio_type|title}}Tils{{key|title}}
+    {{tilstand}} {{oio_type|title}}Tils{{tilstand|title}}
 )
 ;
 {% endfor %}
 
-CREATE TYPE {{oio_type|title}}AttrEgenskaberType AS (
-{% for egenskab in egenskaber %}{{egenskab}} text,
+{%for attribut , attribut_fields in attributter.iteritems() %}
+CREATE TYPE {{oio_type|title}}Attr{{attribut|title}}Type AS (
+{% for field in attribut_fields %}{{field}} text,
  {% endfor %}virkning Virkning
 );
+{% endfor %}
 
 CREATE TYPE {{oio_type|title}}RelationKode AS ENUM  ({% for relation in relationer_nul_til_en|list + relationer_nul_til_mange|list %}'{{relation}}'{% if not loop.last %},{% endif %}{% endfor %});  --WARNING: Changes to enum names requires MANUALLY rebuilding indexes where _actual_state_convert_{{oio_type}}_relation_kode_to_txt is invoked.
 
@@ -27,8 +29,8 @@ CREATE TYPE {{oio_type|title}}RelationType AS (
 
 CREATE TYPE {{oio_type|title}}RegistreringType AS
 (
-registrering RegistreringBase,{% for key, value in tilstande.iteritems() %}
-tils{{key|title}} {{oio_type|title}}Tils{{key|title}}Type[],{% endfor %}
+registrering RegistreringBase,{% for tilstand, tilstand_values in tilstande.iteritems() %}
+tils{{tilstand|title}} {{oio_type|title}}Tils{{tilstand|title}}Type[],{% endfor %}
 attrEgenskaber {{oio_type|title}}AttrEgenskaberType[],
 relationer {{oio_type|title}}RelationType[]
 );
