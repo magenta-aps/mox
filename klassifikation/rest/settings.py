3fca390f455fb008dacddd3134f498b71c56c2a5
Carsten Agger <carstena@magenta.dk>
2015-05-22 13:19:31 +0200

    DB structure in settings file.

--- klassifikation/rest/settings.py
+++ klassifikation/rest/settings.py
@@ -7,4 +7,22 @@ DB_PASSWORD = 'mox'
 
 DATABASE_STRUCTURE = {
 
+    "Facet": { 
+        "attributter": { 
+            "egenskaber" : [
+                "brugervendtnoegle", "beskrivelse", "opbygning", "ophavsret",
+                "plan","supplement","retskilde"
+            ]
+        },
+        "tilstande" : {
+            "publiceret": ["Publiceret", "IkkePubliceret"]
+        },
+        "relationer_nul_til_en" : ["ansvarlig","ejer","facettilhoerer"],
+        "relationer_nul_til_mange" : ["redaktoerer"]
+    },
+    "Klasse": {
+
+    }
+
 }
+
