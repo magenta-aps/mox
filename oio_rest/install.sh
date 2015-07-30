615431dbad41c3abd8e02f0fb680c5f2515f4cf1
Superuser <superuser@bibos2.(none)>
2015-07-30 11:29:39 +0200

    Added new code to install script, not done.

--- oio_rest/install.sh
+++ oio_rest/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 DIR=$(dirname ${BASH_SOURCE[0]})
 VIRTUALENV=./python-env
 
@@ -24,8 +25,33 @@ fi
 
 source $VIRTUALENV/bin/activate
 
+# Database dependencies + installation
+
+
+# Add system user if none exists
+set +x
+getent passwd mox
+if [ $? -ne 0 ]; then 
+    sudo useradd mox
+fi
+set -x
+
+sudo apt-get install postgresql pgxnclient
+sudo pgxn install pgtap
+sudo apt-get install postgresql-contrib
+
+pip install jinja2
+
+pushd $DIR/../db
+./recreatedb.sh
+popd
+
+
 python ./setup.py develop
+
 
 ln -s $VIRTUALENV/bin/oio_api
 
  echo "Run ./oio_api to test API"
+
+
