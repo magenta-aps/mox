72e5dbd99fb22f6d95bb88626159ce84a7fa3577
Seth Yastrov <seth@magenta.dk>
2015-08-05 11:55:56 +0200

    Change REST API list operation to use cast to json instead of to_json function for results.

--- oio_rest/oio_rest/templates/sql/list_objects.sql
+++ oio_rest/oio_rest/templates/sql/list_objects.sql
@@ -1,4 +1,4 @@
-SELECT to_json(a.*) FROM as_list_{{ class_name | lower }}(
+SELECT a::json FROM as_list_{{ class_name | lower }}(
     %(uuid)s::uuid[],
     %(registrering_tstzrange)s,
     %(virkning_tstzrange)s
