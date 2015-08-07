7fd2d2e9b825bea6cb19b948649b09f4c443fcc2
Seth Yastrov <seth@magenta.dk>
2015-08-07 12:33:40 +0200

    Update install.sh for DB installation changes.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ while getopts ":a" OPT; do
 	export AGENT_INSTALL=1
 done
 
-DIR=$(dirname ${BASH_SOURCE[0]})
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 
 # Add system user if none exists
@@ -61,27 +61,22 @@ else
 
 		source $VIRTUALENV/bin/activate
 
-		sudo pgxn install pgtap
 		pip install jinja2
 		sudo -u postgres createuser mox
 
+		# Install pgtap - unit test framework
+		sudo pgxn install pgtap
+
 		# Install pg_amqp - Postgres AMQP extension
-		# pg_amqp installs its pg_amqp.so file to the wrong dir on Ubuntu 14.04,
-		# e.g. /usr/lib/postgresql/9.3/lib/src, instead of
-		# /usr/lib/postgresql/9.3/lib, so we have to move it afterwards
+		# We depend on a specific fork, which supports setting of message headers
+		# https://github.com/duncanburke/pg_amqp.git
+		git clone https://github.com/duncanburke/pg_amqp.git /tmp/pg_amqp
+		cd /tmp/pg_amqp && sudo make install
+		rm -rf /tmp/pg_amqp
 
-		# Grab the Postgres version number, e.g. 9.3, ignoring the 3rd part
-		PG_VERSION_RESULT=$(psql --version)
-		PG_VERSION=`expr "$PG_VERSION_RESULT" : '.*\([0-9]\.[0-9]\)\\.[0-9]$'`
-		sudo mkdir -p "/usr/lib/postgresql/$PG_VERSION/lib/src"
-		sudo pgxn install amqp
-		sudo mv "/usr/lib/postgresql/$PG_VERSION/lib/src/pg_amqp.so" "/usr/lib/postgresql/$PG_VERSION/lib/"
-		sudo rm -d "/usr/lib/postgresql/$PG_VERSION/lib/src/"
-
-
-		pushd $DIR/db
+		cd $DIR/db
 		./recreatedb.sh
-		popd
+		cd $DIR
 
 		deactivate
 	fi
