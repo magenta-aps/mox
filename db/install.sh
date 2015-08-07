14662a636904e87c38421af71581db303bfe0ffb
Seth Yastrov <seth@magenta.dk>
2015-08-07 12:23:28 +0200

    Run DB install.sh again, as it installs a specific fork of the pg_amqp extension (available on github). Update DB change AMQP publish notification to use AMQP headers instead of body. AMQP notifications publish the object type, operation, and uuid.

--- db/install.sh
+++ db/install.sh
@@ -13,14 +13,8 @@ done
 sudo pgxn install pgtap
 
 # Install pg_amqp - Postgres AMQP extension
-# pg_amqp installs its pg_amqp.so file to the wrong dir on Ubuntu 14.04,
-# e.g. /usr/lib/postgresql/9.3/lib/src, instead of
-# /usr/lib/postgresql/9.3/lib, so we have to move it afterwards
-
-# Grab the Postgres version number, e.g. 9.3, ignoring the 3rd part
-PG_VERSION_RESULT=$(psql --version)
-PG_VERSION=`expr "$PG_VERSION_RESULT" : '.*\([0-9]\.[0-9]\)\\.[0-9]$'`
-sudo mkdir -p "/usr/lib/postgresql/$PG_VERSION/lib/src"
-sudo pgxn install amqp
-sudo mv "/usr/lib/postgresql/$PG_VERSION/lib/src/pg_amqp.so" "/usr/lib/postgresql/$PG_VERSION/lib/"
-sudo rm -d "/usr/lib/postgresql/$PG_VERSION/lib/src/"
+# We depend on a specific fork, which supports setting of message headers
+# https://github.com/duncanburke/pg_amqp.git
+git clone https://github.com/duncanburke/pg_amqp.git /tmp/pg_amqp
+cd /tmp/pg_amqp && sudo make install
+rm -rf /tmp/pg_amqp
--- db/recreatedb.sh
+++ db/recreatedb.sh
@@ -28,6 +28,7 @@ sudo -u $MOX_USER psql -d $MOX_DB -U $MOX_USER -f funcs/_as_valid_registrering_l
 sudo -u $MOX_USER psql -d $MOX_DB -U $MOX_USER -f funcs/_as_search_match_array.sql
 sudo -u $MOX_USER psql -d $MOX_DB -U $MOX_USER -f funcs/_json_object_delete_keys.sql
 sudo -u $MOX_USER psql -d $MOX_DB -U $MOX_USER -f funcs/_json_object_delete_keys.sql
+sudo -u $MOX_USER psql -d $MOX_DB -U $MOX_USER -f funcs/_amqp_functions.sql
 
 
 cd ./db-templating/
