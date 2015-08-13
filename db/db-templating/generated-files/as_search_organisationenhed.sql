90f96dd1e6704a7586062d7444ce6b650a268cdb
Thomas Kristensen <thomas@magenta.dk>
2015-08-13 16:22:18 +0200

    Added filtering of returned uuid's in as_search + added test of auth criteria and as_update

--- db/db-templating/generated-files/as_search_organisationenhed.sql
+++ db/db-templating/generated-files/as_search_organisationenhed.sql
@@ -18,7 +18,8 @@ CREATE OR REPLACE FUNCTION as_search_organisationenhed(
 	maxResults int = 2147483647,
 	anyAttrValueArr text[] = '{}'::text[],
 	anyRelUuidArr	uuid[] = '{}'::uuid[],
-	anyRelUrnArr text[] = '{}'::text[]
+	anyRelUrnArr text[] = '{}'::text[],
+	auth_criteria_arr OrganisationenhedRegistreringType[]=null
 	)
   RETURNS uuid[] AS 
 $$
@@ -33,6 +34,7 @@ DECLARE
 	anyAttrValue text;
 	anyRelUuid uuid;
 	anyRelUrn text;
+	auth_filtered_uuids uuid[];
 BEGIN
 
 --RAISE DEBUG 'step 0:registreringObj:%',registreringObj;
@@ -981,7 +983,13 @@ END IF;
 --RAISE DEBUG 'organisationenhed_candidates step 6:%',organisationenhed_candidates;
 
 
-return organisationenhed_candidates;
+										 
+/*** Filter out the objects that does not meets the stipulated access criteria  ***/
+auth_filtered_uuids:=_as_filter_unauth_organisationenhed(organisationenhed_candidates,auth_criteria_arr); 
+/*********************/
+
+
+return auth_filtered_uuids;
 
 
 END;
