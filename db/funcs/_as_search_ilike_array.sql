ae6f4aa7c708a0842fb04523d1fbbb6a431cd111
Thomas Kristensen <thomas@magenta.dk>
2015-08-19 11:19:37 +0200

    Added missing file.

---
+++ db/funcs/_as_search_ilike_array.sql
@@ -0,0 +1,38 @@
+
+-- Copyright (C) 2015 Magenta ApS, http://magenta.dk.
+-- Contact: info@magenta.dk.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+CREATE OR REPLACE FUNCTION _as_search_ilike_array(searchFor text, searchInArr text[])
+RETURNS boolean LANGUAGE plpgsql AS 
+$$
+DECLARE
+
+BEGIN
+
+IF searchFor IS NULL or coalesce(array_length(searchInArr,1),0)=0 THEN  
+	RETURN false;
+ELSE
+	--RAISE NOTICE 'SQL part  searchForArr[%], searchInArr[%]',to_json(searchForArr),to_json(searchInArr);
+	IF EXISTS (
+	SELECT
+	a.searchForElement,
+	b.searchTargetElement
+	FROM
+	unnest(searchInArr) a(searchInElement)
+	WHERE  a.searchInElement ilike a.searchForElement
+	)
+	THEN 
+	RETURN FALSE;
+	ELSE
+	RETURN TRUE;
+	END IF;
+
+END IF;
+
+END;
+$$;
