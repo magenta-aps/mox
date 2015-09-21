5ce33d5aee2514284427d0e4aec2f85336f625dd
Lars Peter Thomsen <larsp@magenta.dk>
2015-09-21 16:00:07 +0200

    Added parameters 'queueUsername' and 'queuePassword' to parameters, and pass them to the amqp interface

--- agent/src/main/java/dk/magenta/mox/agent/MessageInterface.java
+++ agent/src/main/java/dk/magenta/mox/agent/MessageInterface.java
@@ -28,8 +28,17 @@ public abstract class MessageInterface {
 
 
     private static HashMap<String, ConnectionFactory> connectionFactories = new HashMap<String, ConnectionFactory>();
-
     public MessageInterface(String host, String exchange, String queueName) throws IOException {
+        this(null, null, host, exchange, queueName);
+    }
+    public MessageInterface(String username, String password, String host, String exchange, String queueName) throws IOException {
+        if (username == null) {
+            username = ConnectionFactory.DEFAULT_USER;
+        }
+        if (password == null) {
+            password = ConnectionFactory.DEFAULT_PASS;
+        }
+
         this.id = UUID.randomUUID().toString();
         if (!connectionFactories.keySet().contains(host)) {
             ConnectionFactory factory = new ConnectionFactory();
@@ -41,6 +50,9 @@ public abstract class MessageInterface {
             }
             factory.setHost(host);
             factory.setPort(port);
+            System.out.println(username+" "+password);
+            factory.setUsername(username);
+            factory.setPassword(password);
             connectionFactories.put(host, factory);
         }
         if (exchange == null) {
