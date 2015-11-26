44c08192dfcd702169a3f42b4bdbcc02ac018fcb
Lars Peter Thomsen <larsp@magenta.dk>
2015-11-26 09:02:01 +0100

    Refactored security token obtainer to its own class

--- agent/src/main/java/dk/magenta/mox/agent/Main.java
+++ agent/src/main/java/dk/magenta/mox/agent/Main.java
@@ -222,6 +222,8 @@ public class Main {
         }
 
 
+        SecurityTokenObtainer securityTokenObtainer = new SecurityTokenObtainer(properties);
+
         try {
             Map<String, ObjectType> objectTypes = ObjectType.load(propertiesFile);
 
@@ -295,7 +297,7 @@ public class Main {
 
                 String encodedAuthtoken = null;
                 if (properties.getProperty("security.enabled").equalsIgnoreCase("true")) {
-                    String authtoken = getSecurityToken(properties, restInterface);
+                    String authtoken = securityTokenObtainer.getSecurityToken(restInterface);
                     encodedAuthtoken = "saml-gzipped " + base64encode(gzip(authtoken));
                 }
 
@@ -382,7 +384,8 @@ public class Main {
                 String restPassword = String.valueOf(System.console().readPassword("Password:"));
                 properties.setProperty("security.user.name", restUsername);
                 properties.setProperty("security.user.password", restPassword);
-                String authtoken = getSecurityToken(properties, restInterface);
+                
+                String authtoken = securityTokenObtainer.getSecurityToken(restInterface);
                 if (authtoken == null) {
                     System.exit(1);
                 }
@@ -401,127 +404,6 @@ public class Main {
 
     private static JSONObject getJSONObjectFromFilename(String jsonFilename) throws FileNotFoundException, JSONException {
         return new JSONObject(new JSONTokener(new FileReader(new File(jsonFilename))));
-    }
-
-
-
-    private static final String SUBJECT_CONFIRMATION_BEARER = "b";
-    private static final String SUBJECT_CONFIRMATION_HOLDER_OF_KEY = "h";
-    private static final String SAML_TOKEN_TYPE_10 = "1.0";
-    private static final String SAML_TOKEN_TYPE_11 = "1.1";
-    private static final String SAML_TOKEN_TYPE_20 = "2.0";
-
-    public static String getSecurityToken(Properties properties, String endpointAddress) {
-
-        try {
-            String keystorePath = properties.getProperty("security.keystore.path");
-            String keystorePass = properties.getProperty("security.keystore.password");
-            String repoPath = properties.getProperty("security.repo.path");
-
-            System.setProperty("javax.net.ssl.trustStore", keystorePath);
-            System.setProperty("javax.net.ssl.trustStorePassword", keystorePass);
-
-            ConfigurationContext configCtx = ConfigurationContextFactory.createConfigurationContextFromFileSystem(repoPath);
-
-
-            // Create RST Template
-            String tokenType = properties.getProperty("security.saml.token.type");
-            OMFactory omFac = OMAbstractFactory.getOMFactory();
-            OMElement rstTemplate = omFac.createOMElement(SP11Constants.REQUEST_SECURITY_TOKEN_TEMPLATE);
-
-            if (SAML_TOKEN_TYPE_20.equals(tokenType)) {
-                TrustUtil.createTokenTypeElement(RahasConstants.VERSION_05_02, rstTemplate).setText(RahasConstants.TOK_TYPE_SAML_20);
-            } else if (SAML_TOKEN_TYPE_11.equals(tokenType)) {
-                TrustUtil.createTokenTypeElement(RahasConstants.VERSION_05_02, rstTemplate).setText(RahasConstants.TOK_TYPE_SAML_10);
-            }
-
-            String subjectConfirmationMethod = properties.getProperty("security.subject.confirmation.method");
-            if (SUBJECT_CONFIRMATION_BEARER.equals(subjectConfirmationMethod)) {
-                TrustUtil.createKeyTypeElement(RahasConstants.VERSION_05_02, rstTemplate, RahasConstants.KEY_TYPE_BEARER);
-            } else if (SUBJECT_CONFIRMATION_HOLDER_OF_KEY.equals(subjectConfirmationMethod)) {
-                TrustUtil.createKeyTypeElement(RahasConstants.VERSION_05_02, rstTemplate, RahasConstants.KEY_TYPE_SYMM_KEY);
-            }
-
-            // request claims in the token.
-            String claimDialect = properties.getProperty("security.claim.dialect");
-            String[] claimUris = properties.getProperty("security.claim.uris", "").split(",");
-            OMElement claimElement = TrustUtil.createClaims(RahasConstants.VERSION_05_02, rstTemplate, claimDialect);
-            // Populate the <Claims/> element with the <ClaimType/> elements
-
-            OMElement element;
-            // For each and every claim uri, create an <ClaimType/> elem
-            for (String attr : claimUris) {
-                QName qName = new QName("http://schemas.xmlsoap.org/ws/2005/05/identity", "ClaimType", "wsid");
-                element = claimElement.getOMFactory().createOMElement(qName, claimElement);
-                element.addAttribute(claimElement.getOMFactory().createOMAttribute("Uri", null, attr));
-            }
-
-
-            // create STS client
-            STSClient stsClient = new STSClient(configCtx);
-            stsClient.setRstTemplate(rstTemplate);
-
-
-            String action = null;
-            String responseTokenID = null;
-
-            action = TrustUtil.getActionValue(RahasConstants.VERSION_05_02, RahasConstants.RST_ACTION_ISSUE);
-            stsClient.setAction(action);
-
-
-            String stsPolicyPath = properties.getProperty("security.sts.policy.path");
-            StAXOMBuilder omBuilder = new StAXOMBuilder(stsPolicyPath);
-            Policy stsPolicy = PolicyEngine.getPolicy(omBuilder.getDocumentElement());
-
-
-            // Build Rampart config
-            String username = properties.getProperty("security.user.name");
-            String encryptionUsername = properties.getProperty("security.encryption.username");
-            String userCertAlias = properties.getProperty("security.user.cert.alias");
-            String pwdCallbackClass = PasswordCBHandler.class.getCanonicalName();
-
-            RampartConfig rampartConfig = new RampartConfig();
-            rampartConfig.setUser(username);
-            rampartConfig.setEncryptionUser(encryptionUsername);
-            rampartConfig.setUserCertAlias(userCertAlias);
-            rampartConfig.setPwCbClass(pwdCallbackClass);
-
-            Properties cryptoProperties = new Properties();
-            cryptoProperties.put("org.apache.ws.security.crypto.merlin.keystore.type", "JKS");
-            cryptoProperties.put("org.apache.ws.security.crypto.merlin.file", keystorePath);
-            cryptoProperties.put("org.apache.ws.security.crypto.merlin.keystore.password", keystorePass);
-
-            CryptoConfig cryptoConfig = new CryptoConfig();
-            cryptoConfig.setProvider("org.apache.ws.security.components.crypto.Merlin");
-            cryptoConfig.setProp(cryptoProperties);
-
-            rampartConfig.setEncrCryptoConfig(cryptoConfig);
-            rampartConfig.setSigCryptoConfig(cryptoConfig);
-
-            stsPolicy.addAssertion(rampartConfig);
-
-            // request the security token from STS.
-
-            String stsAddress = properties.getProperty("security.sts.address");
-            Token responseToken = stsClient.requestSecurityToken(null, stsAddress, stsPolicy, endpointAddress);
-
-            // store the obtained token in token store to be used in future communication.
-            TokenStorage store = TrustUtil.getTokenStore(configCtx);
-            responseTokenID = responseToken.getId();
-            store.add(responseToken);
-
-            return responseToken.getToken().toString();
-
-        } catch (AxisFault axisFault) {
-            axisFault.printStackTrace();
-        } catch (TrustException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     private static byte[] gzip(String str) throws IOException{
