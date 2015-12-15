ab57967aea0c5e4648d2c3c80cda431af29a8758
Lars Peter Thomsen <larsp@magenta.dk>
2015-12-15 15:59:38 +0100

    Better code layout in ConvertedObject

--- modules/spreadsheet/src/main/java/dk/magenta/mox/spreadsheet/ConvertedObject.java
+++ modules/spreadsheet/src/main/java/dk/magenta/mox/spreadsheet/ConvertedObject.java
@@ -16,12 +16,88 @@ public class ConvertedObject extends HashMap<String, String> {
     private SpreadsheetConversion.SheetData sheet;
     private String id;
     private String operation;
-    private JSONObject generatedJSON;
+    private JSONObject json;
 
     public ConvertedObject(SpreadsheetConversion.SheetData sheet, String id, String operation) {
         this.sheet = sheet;
         this.id = id;
         this.operation = operation;
+        this.json = this.parse();
+    }
+
+    private JSONObject parse() {
+        JSONObject parsed = new JSONObject();
+
+        Set<JSONObject> containers = new HashSet<JSONObject>();
+        JSONObject effectiveObject = new JSONObject();
+
+        for (String key : this.keySet()) {
+            if (!key.equalsIgnoreCase("operation")) {
+                String value = this.get(key);
+                List<String> path = this.sheet.structure.get(key);
+
+                if (path == null) {
+                    log.warn("No structure path for header " + key + " in sheet " + this.sheet.name);
+                } else {
+                    String pathLevel1 = path.get(0);
+
+                    if (pathLevel1.equalsIgnoreCase("virkning")) {
+                        if (key.equalsIgnoreCase("fra")) {
+                            effectiveObject.put("from", value);
+                        } else if (key.equalsIgnoreCase("til")) {
+                            effectiveObject.put("to", value);
+                        }
+                    } else if (path.size() >= 2) {
+                        String pathLevel2 = path.get(1);
+                        List<String> subPath = path.subList(2, path.size());
+                        if (pathLevel1.equalsIgnoreCase("registrering")) {
+                            parsed.put(pathLevel2, value);
+                        } else if (pathLevel1.equalsIgnoreCase("attributter") || pathLevel1.equalsIgnoreCase("tilstande") || pathLevel1.equalsIgnoreCase("relationer")) {
+
+                            if (pathLevel1.equalsIgnoreCase("relationer")) {
+                                String firstSubPath = subPath.isEmpty() ? null : subPath.get(0);
+                                if (firstSubPath != null) {
+                                    if (firstSubPath.equalsIgnoreCase("uuid")) {
+                                        try {
+                                            UUID.fromString(value);
+                                        } catch (IllegalArgumentException e) {
+                                            // It's not a valid uuid
+                                            subPath = new ArrayList<String>(subPath);
+                                            subPath.set(0, "urn");
+                                            value = "urn: " + value;
+                                        }
+                                    } else if (firstSubPath.equalsIgnoreCase("objekttype")) {
+                                        continue;
+                                    }
+                                }
+                            }
+
+                            JSONObject objectLevel1 = parsed.fetchJSONObject(pathLevel1);
+                            JSONArray objectLevel2 = objectLevel1.fetchJSONArray(pathLevel2);
+                            JSONObject container = objectLevel2.fetchJSONObject(0);
+                            containers.add(container);
+
+                            for (int i = 0; i < subPath.size(); i++) {
+                                String pathKey = subPath.get(i);
+                                if (i == subPath.size() - 1) {
+                                    container.put(pathKey, value);
+                                } else {
+                                    container = container.fetchJSONObject(pathKey);
+                                }
+                            }
+                        } else {
+                            log.warn("Unrecognized path: " + this.sheet.name + "." + id + " => " + path + " = " + value + ". Ignoring.");
+                        }
+                    } else {
+                        log.warn("Unrecognized path length: " + this.sheet.name + "." + id + " => " + path + " = " + value + ". Path must have at least two parts.");
+                    }
+                }
+            }
+        }
+        for (JSONObject container : containers) {
+            container.put("virkning", effectiveObject);
+        }
+        return parsed;
     }
 
     public String getOperation() {
@@ -36,80 +112,7 @@ public class ConvertedObject extends HashMap<String, String> {
         return id;
     }
 
-    protected JSONObject toJSON() {
-        if (this.generatedJSON == null) {
-            this.generatedJSON = new JSONObject();
-
-            Set<JSONObject> containers = new HashSet<JSONObject>();
-            JSONObject effectiveObject = new JSONObject();
-
-            for (String key : this.keySet()) {
-                if (!key.equalsIgnoreCase("operation")) {
-                    String value = this.get(key);
-                    List<String> path = this.sheet.structure.get(key);
-
-                    if (path == null) {
-                        log.warn("No structure path for header " + key + " in sheet " + this.sheet.name);
-                    } else {
-                        String pathLevel1 = path.get(0);
-
-                        if (pathLevel1.equalsIgnoreCase("virkning")) {
-                            if (key.equalsIgnoreCase("fra")) {
-                                effectiveObject.put("from", value);
-                            } else if (key.equalsIgnoreCase("til")) {
-                                effectiveObject.put("to", value);
-                            }
-                        } else if (path.size() >= 2) {
-                            String pathLevel2 = path.get(1);
-                            List<String> subPath = path.subList(2, path.size());
-                            if (pathLevel1.equalsIgnoreCase("registrering")) {
-                                this.generatedJSON.put(pathLevel2, value);
-                            } else if (pathLevel1.equalsIgnoreCase("attributter") || pathLevel1.equalsIgnoreCase("tilstande") || pathLevel1.equalsIgnoreCase("relationer")) {
-
-                                if (pathLevel1.equalsIgnoreCase("relationer")) {
-                                    String firstSubPath = subPath.isEmpty() ? null : subPath.get(0);
-                                    if (firstSubPath != null) {
-                                        if (firstSubPath.equalsIgnoreCase("uuid")) {
-                                            try {
-                                                UUID.fromString(value);
-                                            } catch (IllegalArgumentException e) {
-                                                // It's not a valid uuid
-                                                subPath = new ArrayList<String>(subPath);
-                                                subPath.set(0, "urn");
-                                                value = "urn: " + value;
-                                            }
-                                        } else if (firstSubPath.equalsIgnoreCase("objekttype")) {
-                                            continue;
-                                        }
-                                    }
-                                }
-
-                                JSONObject objectLevel1 = this.generatedJSON.fetchJSONObject(pathLevel1);
-                                JSONArray objectLevel2 = objectLevel1.fetchJSONArray(pathLevel2);
-                                JSONObject container = objectLevel2.fetchJSONObject(0);
-                                containers.add(container);
-
-                                for (int i = 0; i < subPath.size(); i++) {
-                                    String pathKey = subPath.get(i);
-                                    if (i == subPath.size() - 1) {
-                                        container.put(pathKey, value);
-                                    } else {
-                                        container = container.fetchJSONObject(pathKey);
-                                    }
-                                }
-                            } else {
-                                log.warn("Unrecognized path: " + this.sheet.name + "." + id + " => " + path + " = " + value + ". Ignoring.");
-                            }
-                        } else {
-                            log.warn("Unrecognized path length: " + this.sheet.name + "." + id + " => " + path + " = " + value + ". Path must have at least two parts.");
-                        }
-                    }
-                }
-            }
-            for (JSONObject container : containers) {
-                container.put("virkning", effectiveObject);
-            }
-        }
-        return this.generatedJSON;
+    public JSONObject getJSON() {
+        return this.json;
     }
 }
