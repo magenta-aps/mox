2667e9c33b3060abe59ba3138ff263f491a9afc2
Lars Peter Thomsen <larsp@magenta.dk>
2016-01-25 17:21:07 +0100

    Fix ClassCastException

--- modules/agent/src/main/java/dk/magenta/mox/agent/messages/Headers.java
+++ modules/agent/src/main/java/dk/magenta/mox/agent/messages/Headers.java
@@ -1,9 +1,16 @@
 package dk.magenta.mox.agent.messages;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by lars on 25-01-16.
  */
 public class Headers extends HashMap<String, Object> {
+    public Headers(){
+        super();
+    }
+    public Headers(Map<String, Object> base) {
+        super(base);
+    }
 }
