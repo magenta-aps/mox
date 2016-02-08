2d18b9c1c93b13d2cdf8cebd17a5ef1f8bc45eff
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-08 17:17:33 +0100

    replace servername on install

--- oio_rest/install.sh
+++ oio_rest/install.sh
@@ -2,9 +2,14 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+SERVERNAME="moxdev.magenta-aps.dk"
+
 sudo mkdir -p /var/www/wsgi
 sudo cp "$DIR/server-setup/oio_rest.wsgi" "/var/www/wsgi/"
 
 sudo cp "$DIR/server-setup/oio_rest.conf" "/etc/apache2/sites-available/"
 sudo a2ensite oio_rest
+
+REPLACENAME="moxtest.magenta-aps.dk"
+sed -i "s/$REPLACENAME/$SERVERNAME" "$DIR/oio_rest/settings.py"
 
