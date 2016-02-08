b31634419b6925b2a2497fd268e0b68397168f66
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-08 16:57:14 +0100

    Skip java installation if it's already there

--- modules/auth/install.sh
+++ modules/auth/install.sh
@@ -2,10 +2,14 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+
 # Ubuntu 14.04 doesn't come with java 8
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
+apt-cache -q=2 show oracle-java8-installer 2>&1 >/dev/null
+if [[ $? > 0 ]]; then
+	sudo add-apt-repository ppa:webupd8team/java
+	sudo apt-get update
+	sudo apt-get -y install oracle-java8-installer
+fi
 
 SYSTEM_PACKAGES=$(cat "$DIR/SYSTEM_DEPENDENCIES")
 for package in "${SYSTEM_PACKAGES[@]}"; do
@@ -16,5 +20,5 @@ cd $DIR
 mvn package
 
 cd "../../"
-ln -s "modules/auth/auth.sh" "auth.sh"
+ln -sf "modules/auth/auth.sh" "auth.sh"
 
