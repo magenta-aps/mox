bc3994de6c5ba85ed7324e933cdd99603ebffd6f
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-15 10:52:59 +0100

    Starting tomcat setup

--- webapp/install.sh
+++ webapp/install.sh
@@ -8,3 +8,19 @@ for package in "${SYSTEM_PACKAGES[@]}"; do
 	sudo apt-get -y install $package
 done
 
+
+
+
+# Install Tomcat apache connector
+SERVERNAME="moxdev.magenta-aps.dk"
+REPLACENAME="moxtest.magenta-aps.dk"
+
+echo "Setting up Tomcat connector for Apache"
+
+sudo cp "$DIR/server-setup/tomcat.conf" "/etc/apache2/sites-available/"
+sed -i "s/$REPLACENAME/$SERVERNAME/" "/etc/apache2/sites-available/tomcat.conf"
+sudo a2ensite tomcat
+
+
+sudo mkdir -p /var/log/mox/oio_rest
+
