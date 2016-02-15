3bb54573c351cb697d9e012d3fe9f2387217892f
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-15 13:18:31 +0100

    Be sure to compile the agents

--- agents/MoxRestFrontend/install.sh
+++ agents/MoxRestFrontend/install.sh
@@ -4,3 +4,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 sudo cp "$DIR/setup/moxtabel.conf" /etc/init/
 
+pushd $DIR
+mvn package
+popd
+
+sudo mkdir -p /var/log/mox
+sudo touch /var/log/mox/moxrestfrontend.log
+
