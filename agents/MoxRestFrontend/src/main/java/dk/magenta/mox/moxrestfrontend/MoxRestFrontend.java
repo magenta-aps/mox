3e6194e8c763bca8ac8125a41e57ee535a7f2b9f
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-19 14:25:49 +0100

    Add shutdown hook

--- agents/MoxRestFrontend/src/main/java/dk/magenta/mox/moxrestfrontend/MoxRestFrontend.java
+++ agents/MoxRestFrontend/src/main/java/dk/magenta/mox/moxrestfrontend/MoxRestFrontend.java
@@ -56,4 +56,6 @@ public class MoxRestFrontend extends MoxObjectAgent {
         }
         log.info("--------------------------------------------------------------------------------\n");
     }
+
+
 }
