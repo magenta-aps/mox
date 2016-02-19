b61b6043a3a9011a7ea0ddc43205b9ff9a9173cc
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-19 13:54:43 +0100

    Run agents as the mox user

--- agents/MoxRestFrontend/moxrestfrontend.sh
+++ agents/MoxRestFrontend/moxrestfrontend.sh
@@ -2,13 +2,11 @@
 
 DIR=$(dirname ${BASH_SOURCE[0]})
 
-if [[ -z $@ ]]; then
-  args="listen"
-else
-  args="$@"
+PRECMD=""
+if [[ `whoami` ne "mox" ]]; then
+    PRECMD="sudo su mox "
 fi
 
-
 cd $DIR
-java -cp "target/MoxRestFrontend-1.0.jar:target/dependency/*" dk.magenta.mox.moxrestfrontend.MoxRestFrontend $args
+$PRECMD java -cp "target/MoxRestFrontend-1.0.jar:target/dependency/*" dk.magenta.mox.moxrestfrontend.MoxRestFrontend
 cd - > /dev/null
