f962af7bf1e9898e0b0b70c7c346a368eb1d8018
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-26 11:58:57 +0100

    Set up correct logging

--- servlets/MoxDocumentUpload/install.sh
+++ servlets/MoxDocumentUpload/install.sh
@@ -4,6 +4,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 WARFILE="MoxDocumentUpload.war"
 
+sudo mkdir -p /var/log/mox
+sudo touch /var/log/mox/moxdocumentupload.log
+sudo chown tomcat7 /var/log/mox/moxdocumentupload.log
+
 # Compile and install servlet
 pushd $DIR
 mvn package
