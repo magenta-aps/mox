c20020c64bb023f502595518363eb65eeec58018
Lars Peter Thomsen <larsp@magenta.dk>
2016-06-24 14:04:08 +0200

    Add install for production

--- oio_rest/install.sh
+++ oio_rest/install.sh
@@ -134,7 +134,7 @@ echo "Setting up oio_rest WSGI service for Apache"
 sudo mkdir -p /var/www/wsgi
 sudo cp "$DIR/server-setup/oio_rest.wsgi" "/var/www/wsgi/"
 
-sudo cp "$DIR/server-setup/oio_rest.conf" "/etc/apache2/sites-available/"
+sudo cp "$DIR/server-setup/oio_rest.conf.production" "/etc/apache2/sites-available/"
 sudo a2ensite oio_rest
 sudo a2enmod ssl
 sudo a2enmod cgi
