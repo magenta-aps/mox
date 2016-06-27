455301b9579e35f28845c3de810e04a5a3de8829
Lars Peter Thomsen <larsp@magenta.dk>
2016-06-27 16:10:02 +0200

    Remove obsolete debug comments

--- modules/spreadsheet/src/main/java/dk/magenta/mox/spreadsheet/Structure.java
+++ modules/spreadsheet/src/main/java/dk/magenta/mox/spreadsheet/Structure.java
@@ -105,12 +105,7 @@ public class Structure {
     }
 
     public JSONObject addConversion(JSONObject base, String key, String value) {
-        //System.out.println("------------------------");
-        //System.out.println("base: "+base);
-        //System.out.println("key: "+key);
-        //System.out.println("value: "+value);
         StructurePath path = this.getConversionPath(key);
-        //System.out.println("path: "+path);
         JSONObject ptr = base;
         for (int i=0; i<path.size(); i++) {
 
@@ -119,15 +114,12 @@ public class Structure {
             if (pathKey.contains("|")) {
                 pathKey = this.interpretPathBranch(pathKey, value);
             }
-            //System.out.println("pathKey: "+pathKey);
 
             if (i == path.size()-1) {
                 ptr.put(pathKey, value);
             } else {
                 if (this.isList(subPath)) {
-                    //System.out.println(subPath+" is a list");
                     JSONArray arr = ptr.fetchJSONArray(pathKey);
-                    //System.out.println("fetched "+arr+" from "+ptr);
                     if (arr.length() > 0) {
                         ptr = arr.getJSONObject(0);
                     } else {
@@ -138,8 +130,6 @@ public class Structure {
                     ptr = ptr.fetchJSONObject(pathKey);
                 }
             }
-            //System.out.println("ptr: "+ptr);
-            //System.out.println("------");
         }
         return ptr;
     }
@@ -159,7 +149,6 @@ public class Structure {
         } else if (!structuresFile.canRead()) {
             log.error("File "+structuresFile+" is not readable");
         } else {
-            // JSONObject structures = new JSONObject("{\"klasse\":{\"Kontekst\":[\"kontekst\",\"klasseegenskaber\",\"facet\"],\"LovligeKombinationer\":[\"relationer\",\"lovligekombinationer\",\"uuid\"],\"Erstatter\":[\"relationer\",\"erstatter\",\"uuid\"],\"Søgeord_beskrivelse\":[\"attributter\",\"klasseegenskaber\",\"soegeord\",\"beskrivelse\"],\"Aendringsnotat\":[\"attributter\",\"klasseegenskaber\",\"aendringsnotat\"],\"Eksempel\":[\"attributter\",\"klasseegenskaber\",\"eksempel\"],\"Redaktoerer_type\":[\"relationer\",\"redaktoerer\",\"objekttype\"],\"Operation\":[\"registrering\",\"operation\"],\"Omfang\":[\"attributter\",\"klasseegenskaber\",\"omfang\"],\"Mapninger\":[\"relationer\",\"mapninger\",\"uuid\"],\"Beskrivelse\":[\"attributter\",\"klasseegenskaber\",\"beskrivelse\"],\"Ejer\":[\"relationer\",\"ejer\",\"uuid\"],\"BrugervendtNoegle\":[\"attributter\",\"klasseegenskaber\",\"brugervendtnoegle\"],\"Retskilde\":[\"attributter\",\"klasseegenskaber\",\"retskildetekst\"],\"Ejer_type\":[\"relationer\",\"ejer\",\"objekttype\"],\"Facet\":[\"relationer\",\"facet\",\"uuid\"],\"Søgeord_kategori\":[\"attributter\",\"klasseegenskaber\",\"soegeord\",\"kategori\"],\"Tilfoejelser\":[\"relationer\",\"tilfoejelser\",\"uuid\"],\"Sideordnede\":[\"relationer\",\"sideordnede\",\"uuid\"],\"Søgeord\":[\"attributter\",\"klasseegenskaber\",\"soegeord\",\"soegeord\"],\"OverordnetKlasse\":[\"relationer\",\"overordnetklasse\",\"uuid\"],\"Redaktoerer\":[\"relationer\",\"redaktoerer\",\"uuid\"],\"Facet_type\":[\"relationer\",\"facet\",\"objekttype\"],\"Ansvarlig_type\":[\"relationer\",\"ansvarlig\",\"objekttype\"],\"Fra\":[\"virkning\"],\"Til\":[\"virkning\"],\"Titel\":[\"attributter\",\"klasseegenskaber\",\"titel\"],\"Note\":[\"registrering\",\"note\"],\"Publiceret\":[\"tilstande\",\"publiceret\"],\"objektID\":[\"registrering\",\"id\"],\"Ansvarlig\":[\"relationer\",\"ansvarlig\",\"uuid\"]},\"klassifikation\":{\"Kontekst\":[\"kontekst\",\"klassifikationegenskaber\",\"ejer\"],\"Kaldenavn\":[\"attributter\",\"klassifikationegenskaber\",\"kaldenavn\"],\"Operation\":[\"registrering\",\"operation\"],\"Ophavsret\":[\"attributter\",\"klassifikationegenskaber\",\"ophavsret\"],\"Beskrivelse\":[\"attributter\",\"klassifikationegenskaber\",\"beskrivelse\"],\"Ejer\":[\"relationer\",\"ejer\",\"uuid\"],\"Ansvarlig_type\":[\"relationer\",\"ansvarlig\",\"objekttype\"],\"Fra\":[\"virkning\"],\"BrugervendtNoegle\":[\"attributter\",\"klassifikationegenskaber\",\"brugervendtnoegle\"],\"Til\":[\"virkning\"],\"Note\":[\"registrering\",\"note\"],\"Publiceret\":[\"tilstande\",\"klassifikationpubliceret\",\"publiceret\"],\"objektID\":[\"registrering\",\"id\"],\"Ejer_type\":[\"relationer\",\"ejer\",\"objekttype\"],\"Ansvarlig\":[\"relationer\",\"ansvarlig\",\"uuid\"]},\"facet\":{\"Kontekst\":[\"kontekst\",\"facetegenskaber\",\"facettilhoerer\"],\"Redaktoerer_type\":[\"relationer\",\"redaktoerer\",\"objekttype\"],\"FacetTilhoerer_type\":[\"relationer\",\"facettilhoerer\",\"objekttype\"],\"Operation\":[\"registrering\",\"operation\"],\"PlanIdentifikator\":[\"attributter\",\"facetegenskaber\",\"planidentifikator\"],\"Ophavsret\":[\"attributter\",\"facetegenskaber\",\"ophavsret\"],\"Redaktoerer\":[\"relationer\",\"redaktoerer\",\"uuid\"],\"Beskrivelse\":[\"attributter\",\"facetegenskaber\",\"beskrivelse\"],\"Ejer\":[\"relationer\",\"ejer\",\"uuid\"],\"FacetTilhoerer\":[\"relationer\",\"facettilhoerer\",\"uuid\"],\"Ansvarlig_type\":[\"relationer\",\"ansvarlig\",\"objekttype\"],\"Fra\":[\"virkning\"],\"BrugervendtNoegle\":[\"attributter\",\"facetegenskaber\",\"brugervendtnoegle\"],\"Til\":[\"virkning\"],\"Note\":[\"registrering\",\"note\"],\"Supplement\":[\"attributter\",\"facetegenskaber\",\"supplement\"],\"Publiceret\":[\"tilstande\",\"facetpublicering\",\"publiceret\"],\"Retskilde\":[\"attributter\",\"facetegenskaber\",\"retskilde\"],\"objektID\":[\"registrering\",\"id\"],\"Ejer_type\":[\"relationer\",\"ejer\",\"objekttype\"],\"Opbygning\":[\"attributter\",\"facetegenskaber\",\"opbygning\"],\"Ansvarlig\":[\"relationer\",\"ansvarlig\",\"uuid\"]}}");
             try {
                 JSONObject structures = new JSONObject(structuresFile);
                 for (String sheetname : structures.keySet()) {
