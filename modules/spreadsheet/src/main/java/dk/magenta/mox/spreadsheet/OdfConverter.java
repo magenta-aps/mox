b46247abf60121ee1c20c6e65bdf38af19a386cf
Lars Peter Thomsen <larsp@magenta.dk>
2016-06-27 14:26:23 +0200

    Fix ods import

--- modules/spreadsheet/src/main/java/dk/magenta/mox/spreadsheet/OdfConverter.java
+++ modules/spreadsheet/src/main/java/dk/magenta/mox/spreadsheet/OdfConverter.java
@@ -1,9 +1,13 @@
 package dk.magenta.mox.spreadsheet;
-
+/*
 import org.odftoolkit.simple.SpreadsheetDocument;
 import org.odftoolkit.simple.table.Cell;
 import org.odftoolkit.simple.table.Row;
 import org.odftoolkit.simple.table.Table;
+*/
+import org.apache.log4j.Logger;
+
+import org.jopendocument.dom.spreadsheet.*;
 
 import java.io.File;
 import java.io.InputStream;
@@ -13,12 +17,55 @@ import java.io.InputStream;
  */
 public class OdfConverter extends SpreadsheetConverter {
 
+    private static Logger log = Logger.getLogger(SpreadsheetConverter.class);
+
     protected String[] getApplicableContentTypes() {
         return new String[]{
                 "application/vnd.oasis.opendocument.spreadsheet"
         };
     };
 
+    public SpreadsheetConversion convert(File data) throws Exception {
+        return this.convert(SpreadSheet.createFromFile(data));
+    }
+
+    private SpreadsheetConversion convert(SpreadSheet document) throws Exception {
+        SpreadsheetConversion spreadsheetConversion = new SpreadsheetConversion();
+        int sheetCount = document.getSheetCount();
+        for (int i = 0; i < sheetCount; i++) {
+            Sheet sheet = document.getSheet(i);
+            String sheetName = sheet.getName();
+            int rowCount = sheet.getRowCount();
+            int columnCount = sheet.getColumnCount();
+            int emptyRows = 0;
+            for (int j = 0; j < rowCount; j++) {
+                SpreadsheetRow rowData = new SpreadsheetRow(columnCount);
+                for (int k = 0; k < columnCount; k++) {
+                    Cell cell;
+                    cell = sheet.getImmutableCellAt(k, j);
+                    String contents = getCellString(cell);
+                    if (contents == null || contents.isEmpty()) {
+                        emptyRows++;
+                    } else {
+                        emptyRows = 0;
+                    }
+                    rowData.add(contents);
+                }
+                if (emptyRows == 0) {
+                    spreadsheetConversion.addRow(sheetName, rowData, j == 0);
+                } else if (emptyRows >= 10) {
+                    break;
+                }
+            }
+        }
+        return spreadsheetConversion;
+    }
+
+    private static String getCellString(Cell cell) {
+        return cell.getTextValue();
+    }
+
+/*
     public SpreadsheetConversion convert(InputStream data) throws Exception {
         return this.convert(SpreadsheetDocument.loadDocument(data));
     }
@@ -27,6 +74,7 @@ public class OdfConverter extends SpreadsheetConverter {
     }
 
     private SpreadsheetConversion convert(SpreadsheetDocument document) throws Exception {
+        System.out.println("Converting");
         SpreadsheetConversion spreadsheetConversion = new SpreadsheetConversion();
         int sheetCount = document.getSheetCount();
         for (int i = 0; i < sheetCount; i++) {
@@ -53,4 +101,5 @@ public class OdfConverter extends SpreadsheetConverter {
         }
         return cell.getStringValue();
     }
+*/
 }
