0dc874897d960b76406f1c94424cac735ef58148
Lars Peter Thomsen <larsp@magenta.dk>
2016-07-05 10:14:51 +0200

    Decrease verbosity in java compilation

--- modules/spreadsheet/install.sh
+++ modules/spreadsheet/install.sh
@@ -2,10 +2,10 @@
 
 echo "Compiling spreadsheet module"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-pushd "$DIR"
+pushd "$DIR" > /dev/null
 
-mvn package
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=target/spreadsheet-1.0.jar -DgroupId=dk.magenta.mox -DartifactId=spreadsheet -Dversion=1.0 -Dpackaging=jar -DlocalRepositoryPath=$HOME/.m2/repository
+mvn package --quiet -Dmaven.test.skip=true
+mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file --quiet -Dfile=target/spreadsheet-1.0.jar -DgroupId=dk.magenta.mox -DartifactId=spreadsheet -Dversion=1.0 -Dpackaging=jar -DlocalRepositoryPath=$HOME/.m2/repository
 
-popd
+popd > /dev/null
 
