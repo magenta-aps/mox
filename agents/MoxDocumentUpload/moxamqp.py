78ae885e7f97b4026f6d077a5659d587b0803363
Lars Peter Thomsen <larsp@magenta.dk>
2016-07-15 11:10:20 +0200

    Let client query for updates

--- agents/MoxDocumentUpload/moxamqp.py
+++ agents/MoxDocumentUpload/moxamqp.py
@@ -1,9 +1,8 @@
 import amqp
 import uuid
 import json
-import datetime
-
 import time
+from custom_exceptions import NoSuchJob
 
 class Message(object):
 
@@ -83,7 +82,7 @@ class MessageInterface(object):
         self.connection.connect()
         self.channel = amqp.Channel(self.connection)
         self.channel.open()
-        self.queueResult = self.channel.queue_declare(queue)
+        self.channel.queue_declare(queue, durable=False, exclusive=False, auto_delete=False)
 
 
 
@@ -97,7 +96,6 @@ class MessageSender(MessageInterface):
 
     def __init__(self, username, password, host, queue, exchange=''):
         super(MessageSender, self).__init__(username, password, host, queue, exchange)
-        self.replyConsumer = self.channel.basic_consume(callback=self.handleResponse)
 
     def setAppId(self, appId):
         self.appId = appId
@@ -114,24 +112,23 @@ class MessageSender(MessageInterface):
 
         return properties
 
-    def send(self, message, expectReply=False):
+    def send(self, message):
         data = json.dumps(message.getData())
         properties = self.getStandardProperties()
         properties['application_headers'] = message.getHeaders()
+        replyQueue = str(uuid.uuid4())
+        properties['correlation_id'] = replyQueue
+        properties['reply_to'] = replyQueue
         amqpMessage = amqp.basic_message.Message(data, **properties)
 
         self.channel.basic_publish(amqpMessage, self.exchange, self.queue)
+        self.channel.queue_declare(replyQueue, durable=False, exclusive=True, auto_delete=True)
+        return replyQueue
 
-    def handleResponse(self, amqpMessage):
-        print "response: %s" % str(amqpMessage)
-
-
-"""
-def test():
-    sender = MessageSender("guest", "guest", "localhost", "testqueue")
-    message = UploadedDocumentMessage(uuid.uuid4(), "self-written authkey")
-    sender.send(message)
-    time.sleep(5)
-
-test()
-"""
+    def getJobStatus(self, replyQueue):
+        try:
+            reply = self.channel.basic_get(replyQueue)
+        except amqp.exceptions.NotFound:
+            raise NoSuchJob(replyQueue)
+        if reply is not None:
+            return reply.body
