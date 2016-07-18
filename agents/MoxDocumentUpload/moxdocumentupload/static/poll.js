25e8ac2eccf90b2b1e2e826b7c655c6889d85865
Lars Peter Thomsen <larsp@magenta.dk>
2016-07-18 16:02:08 +0200

    Move poll function out to static file

---
+++ agents/MoxDocumentUpload/moxdocumentupload/static/poll.js
@@ -0,0 +1,33 @@
+/**
+ * Created by lars on 18-07-16.
+ */
+var pollJob = function(jobId, callback) {
+    if (jobId && callback) {
+        var poller = setInterval(function(){
+            $.get({
+                url: "status",
+                data: {'jobId': jobId},
+                dataType: "json",
+                success: function(responseObject, status, jqXHR){
+                    if (responseObject && 'response' in responseObject) {
+                        var serviceResponse = responseObject.response;
+                        if (typeof(serviceResponse)=="string") {
+                            try {
+                                serviceResponse = JSON.parse(serviceResponse);
+                            } catch (e) {}
+                        }
+                        if (typeof(serviceResponse)=="object") {
+                            callback(serviceResponse);
+                        }
+                        clearInterval(poller);
+                        poller = null;
+                    }
+                },
+                error: function(){
+                    clearInterval(poller);
+                    poller = null;
+                }
+            });
+        }, 1000);
+    }
+};
