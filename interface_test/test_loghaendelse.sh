515a98977ecf6a6c49a50eef9656609b48758d5e
Carsten Agger <carstena@magenta.dk>
2016-10-27 15:55:29 +0200

    Test script for adding and retrieving log element.

---
+++ interface_test/test_loghaendelse.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# In order to start the tests, make sure the REST interface is running.
+#
+# At present, you do that by activating the virtualenv and running the
+# command
+#    python app.py
+#
+# Then run the present script in a different terminal.
+
+# The purpose of these tests is to run through the full CRUD scenario in
+# the REST API for all classes in the hierarchy.
+
+# First, create a new facet.
+set -x
+# Test configuration
+DIR=$(dirname ${BASH_SOURCE[0]})
+source $DIR/config.sh
+
+result=$(curl -k -H "Content-Type: application/json" -X POST -d "$(cat $DIR/test_data/loghaendelse_opret.json)" $HOST_URL/log/loghaendelse)
+
+echo "RESULT: $result"
+
+uuid=$(expr "$result" : '.*"uuid": "\([^"]*\)"')
+echo "Oprettet LogHændelse: $uuid"
+# Now, import a new facet
+# - Suppose no object with this ID exists.
+import_uuid=$(uuidgen)
+
+
+#curl -k -sH "Content-Type: application/json" -X PUT -d "$(cat $DIR/test_data/klasse_opdater.json)" $HOST_URL/klassifikation/klasse/$uuid
+
+
+
+# List klasser
+
+curl -k -sH "Content-Type: application/json" -X GET $HOST_URL/log/loghaendelse?uuid=$uuid 
+
