d8443053d462d25d2f778b840c2695aa4dd28a20
Thomas Kristensen <tk@openmodus.dk>
2016-11-20 13:07:40 +0100

    OIO-tilstand: Tilføjede tests vedr. update-funktionen.

--- db/tests/test_as_update_tilstand.sql
+++ db/tests/test_as_update_tilstand.sql
@@ -17,14 +17,17 @@ DECLARE
 	virkEgenskaber2 Virkning;
 	virkTilstandsobjekt Virkning;
 	virkTilstandsvaerdi1 Virkning;
+	virkTilstandsvaerdi1D Virkning;
 	virkTilstandsvaerdi2 Virkning;
 	virkTilstandskvalitet1 Virkning;
 	virkTilstandskvalitet2 Virkning;
 	virkPubliceret Virkning;
 	virkStatus Virkning;
+	virkStatus2 Virkning;
 	tilstandEgenskab tilstandEgenskaberAttrType;
 	tilstandEgenskab2 tilstandEgenskaberAttrType;
 	tilstandStatus tilstandStatusTilsType;
+	tilstandStatus2 tilstandStatusTilsType;
 	tilstandPubliceret tilstandPubliceretTilsType;
 	tilstandRelTilstandsobjekt tilstandRelationType;
 	tilstandRelTilstandsvaerdi1 tilstandRelationType;
@@ -43,6 +46,8 @@ DECLARE
 	uuidRegistrering uuid :='1f368584-4c3e-4ba4-837b-da2b1eee37c9'::uuid;
 	uuidVirkEgenskaber uuid :='6f368584-4c3e-4ba4-837b-da2b1eee37ca'::uuid;
 	uuidVirkEgenskaber2 uuid :='9f368584-4c3e-4ba4-837b-da2b1eee37cb'::uuid;
+	uuidVirkStatus2 uuid:='e470ce13-a546-4940-a1a8-016e4e2197de'::uuid;
+	uuidVirkStatus uuid:='cd50aea8-9bef-4914-818c-8f2cfdf42ddf'::uuid;
 	actual_publiceret_virk virkning;
 	actual_publiceret_value tilstandStatusTils;
 	actual_publiceret tilstandStatusTilsType;
@@ -50,9 +55,20 @@ DECLARE
 	uuid_to_import uuid :='a1819cce-043b-447f-ba5e-92e6a75df918'::uuid;
 	uuid_returned_from_import uuid;
 	read_Tilstand1 TilstandType;
+	read_Tilstand2 TilstandType;
+	read_Tilstand3 TilstandType;
+	read_Tilstand4 TilstandType;
+
 	expected_tilstand1 TilstandType;
+	expected_tilstand2 TilstandType;
+	expected_tilstand3 TilstandType;
+	expected_tilstand4 TilstandType;
 
 	updated_reg_id_1 bigint;
+	updated_reg_id_2 bigint;
+	updated_reg_id_3 bigint;
+	updated_reg_id_4 bigint;
+
 BEGIN
 
 
@@ -99,7 +115,7 @@ virkPubliceret := ROW (
 
 virkstatus := ROW (
 	'[2016-12-18, infinity)' :: TSTZRANGE,
-          uuid_generate_v4(),
+          uuidVirkStatus,
           'Bruger',
           'NoteEx20'
 ) :: Virkning
@@ -359,14 +375,302 @@ RETURN NEXT IS(
 
 /*****************************************************************/
 
+virkstatus2 := ROW (
+	'[2017-01-01, 2017-01-31]' :: TSTZRANGE,
+          uuidVirkStatus2,
+          'Bruger',
+          'NoteEx30'
+) :: Virkning
+;
+
+tilstandStatus2 := ROW (
+virkStatus2,
+'Inaktiv'::TilstandStatusTils
+):: tilstandStatusTilsType
+;
 
 
 
+updated_reg_id_2:=as_update_tilstand(
+  new_uuid1,
+  uuidRegistrering,
+  'Test Note 28',
+  'Rettet'::Livscykluskode,           
+  null, --ARRAY[tilstandEgenskab2] :: TilstandEgenskaberAttrType[],
+   array[tilstandStatus2], --tilsStatus TilstandStatusTilsType[],
+  null, -- tilsPubliceret TilstandPubliceretTilsType[],
+  null --relationer TilstandRelationType[]
+	);
+
+
+read_Tilstand2 := as_read_tilstand(new_uuid1,
+	null, --registrering_tstzrange
+	null --virkning_tstzrange
+	);
+
+--raise notice 'read_Tilstand2:%',to_json(read_Tilstand2);
 
 
 
+expected_tilstand2:=ROW(
+		new_uuid1,
+		ARRAY[
+			ROW(
+			(read_Tilstand2.registrering[1]).registrering
+			,ARRAY[
+				tilstandStatus2,
+				ROW (
+					ROW(
+				'[2016-12-18, 2017-01-01)' :: TSTZRANGE,
+				uuidVirkStatus,
+				'Bruger',
+				'NoteEx20'
+				) :: Virkning,
+				'Aktiv'::TilstandStatusTils
+				):: tilstandStatusTilsType
+				,
+				ROW (
+					ROW(
+				'(2017-01-31, infinity)' :: TSTZRANGE,
+				uuidVirkStatus,
+				'Bruger',
+				'NoteEx20'
+				) :: Virkning,
+				'Aktiv'::TilstandStatusTils
+				):: tilstandStatusTilsType
+				]::tilstandStatusTilsType[]
+			,(expected_tilstand1.registrering[1]).tilsPubliceret
+			,(expected_tilstand1.registrering[1]).attrEgenskaber 
+			,(expected_tilstand1.registrering[1]).relationer
+			)::TilstandRegistreringType
+			]::TilstandRegistreringType[]
+		)::TilstandType
+;
+
+--raise notice 'expected_tilstand2:%',to_json(expected_tilstand2);
+
+RETURN NEXT IS(
+	read_Tilstand2,
+	expected_tilstand2
+	,'test update tilstand #2'
+);
+
+/***************************************************/
 
 
+updated_reg_id_3:=as_update_tilstand(
+  new_uuid1,
+  uuidRegistrering,
+  'Test Note 45',
+  'Rettet'::Livscykluskode,           
+  null, --ARRAY[tilstandEgenskab2] :: TilstandEgenskaberAttrType[],
+  null, --tilsStatus TilstandStatusTilsType[],
+  null, -- tilsPubliceret TilstandPubliceretTilsType[],
+  ARRAY[ ROW (
+					'tilstandsvaerdi'::tilstandRelationKode,
+						virkTilstandsvaerdi1,
+					null,
+					null,
+					null
+					,1 
+					,ROW(true,'85')::TilstandsVaerdiRelationAttrType ) ]::TilstandRelationType[]  --relationer TilstandRelationType[]
+	);
+
+read_Tilstand3 := as_read_tilstand(new_uuid1,
+	null, --registrering_tstzrange
+	null --virkning_tstzrange
+	);
+
+--raise notice 'read_Tilstand3:%',to_json(read_Tilstand3);
+
+
+
+expected_tilstand3:=ROW(
+		new_uuid1,
+		ARRAY[
+			ROW(
+			(read_Tilstand3.registrering[1]).registrering
+			,(expected_tilstand2.registrering[1]).tilsStatus
+			,(expected_tilstand2.registrering[1]).tilsPubliceret
+			,(expected_tilstand2.registrering[1]).attrEgenskaber 
+			,ARRAY[
+				ROW (
+				'tilstandsvaerdi'::tilstandRelationKode,
+					virkTilstandsvaerdi2,
+				uuidTilstandsvaerdi2,
+				null,--urnTilstandsvaerdi2
+				'Klasse'
+				,2 --NOTICE: Was replace din by import function
+				, ROW(null,null)::TilstandsVaerdiRelationAttrType --will be removed in python-layer
+			) :: tilstandRelationType
+			,
+				ROW (
+				'tilstandskvalitet'::tilstandRelationKode,
+					virkTilstandskvalitet2,
+				uuidTilstandskvalitet2,
+				null,
+				'Klasse'
+				,2 --NOTICE: Was replaced by import function
+				,ROW(null,null)::TilstandsVaerdiRelationAttrType --will be removed in python-layer
+			) :: tilstandRelationType
+			,
+				ROW (
+				'tilstandskvalitet'::tilstandRelationKode,
+					virkTilstandskvalitet1,
+				uuidTilstandskvalitet1,
+				null,
+				'Klasse'
+				,1 --NOTICE: Was replaced  by import function
+				,ROW(null,null)::TilstandsVaerdiRelationAttrType --will be removed in python-layer
+			) :: tilstandRelationType
+			,
+				ROW (
+				'tilstandsobjekt'::tilstandRelationKode
+				,virkTilstandsobjekt
+				,uuidTilstandsobjekt
+				,null
+				,'Person'
+				,NULL --NOTICE: Was replaced
+				,ROW(null,null)::TilstandsVaerdiRelationAttrType --will be removed in python-layer
+			) :: tilstandRelationType
+				,
+				 ROW (
+					'tilstandsvaerdi'::tilstandRelationKode,
+						virkTilstandsvaerdi1,
+					null,
+					null,
+					null
+					,1 --NOTICE: Was replaced by import function
+					,ROW(true,'85')::TilstandsVaerdiRelationAttrType
+				) :: tilstandRelationType
+			
+				]::TilstandRelationType[]
+
+
+			)::TilstandRegistreringType
+			]::TilstandRegistreringType[]
+		)::TilstandType
+;
+
+--raise notice 'expected_tilstand3:%',to_json(expected_tilstand3);
+
+RETURN NEXT IS(
+	read_Tilstand3,
+	expected_tilstand3
+	,'test update tilstand #3'
+);
+
+/***********************************************/
+
+virkTilstandsvaerdi1D :=	ROW (
+	'[2015-05-10, infinity)' :: TSTZRANGE,
+          uuid_generate_v4(),
+          'Bruger',
+          'Værdien blev slettet pga. fejl i indberetning.'
+          ) :: Virkning
+;
+
+updated_reg_id_4:=as_update_tilstand(
+  new_uuid1,
+  uuidRegistrering,
+  'Test Note 50',
+  'Rettet'::Livscykluskode,           
+  null, --ARRAY[tilstandEgenskab2] :: TilstandEgenskaberAttrType[],
+  null, --tilsStatus TilstandStatusTilsType[],
+  null, -- tilsPubliceret TilstandPubliceretTilsType[],
+  ARRAY[ ROW (
+					'tilstandsvaerdi'::tilstandRelationKode,
+						virkTilstandsvaerdi1D,
+					null,
+					null,
+					null
+					,1 
+					,ROW(null,null)::TilstandsVaerdiRelationAttrType ) ]::TilstandRelationType[]  --relationer TilstandRelationType[]
+	);
+
+read_Tilstand4 := as_read_tilstand(new_uuid1,
+	null, --registrering_tstzrange
+	null --virkning_tstzrange
+	);
+
+raise notice 'read_Tilstand4:%',to_json(read_Tilstand4);
+
+expected_tilstand4:=ROW(
+		new_uuid1,
+		ARRAY[
+			ROW(
+			(read_Tilstand4.registrering[1]).registrering
+			,(expected_tilstand2.registrering[1]).tilsStatus
+			,(expected_tilstand2.registrering[1]).tilsPubliceret
+			,(expected_tilstand2.registrering[1]).attrEgenskaber 
+			,ARRAY[
+				ROW (
+				'tilstandsvaerdi'::tilstandRelationKode,
+					virkTilstandsvaerdi2,
+				uuidTilstandsvaerdi2,
+				null,--urnTilstandsvaerdi2
+				'Klasse'
+				,2 --NOTICE: Was replace din by import function
+				, ROW(null,null)::TilstandsVaerdiRelationAttrType --will be removed in python-layer
+			) :: tilstandRelationType
+			,
+				ROW (
+				'tilstandskvalitet'::tilstandRelationKode,
+					virkTilstandskvalitet2,
+				uuidTilstandskvalitet2,
+				null,
+				'Klasse'
+				,2 --NOTICE: Was replaced by import function
+				,ROW(null,null)::TilstandsVaerdiRelationAttrType --will be removed in python-layer
+			) :: tilstandRelationType
+			,
+				ROW (
+				'tilstandskvalitet'::tilstandRelationKode,
+					virkTilstandskvalitet1,
+				uuidTilstandskvalitet1,
+				null,
+				'Klasse'
+				,1 --NOTICE: Was replaced  by import function
+				,ROW(null,null)::TilstandsVaerdiRelationAttrType --will be removed in python-layer
+			) :: tilstandRelationType
+			,
+				ROW (
+				'tilstandsobjekt'::tilstandRelationKode
+				,virkTilstandsobjekt
+				,uuidTilstandsobjekt
+				,null
+				,'Person'
+				,NULL --NOTICE: Was replaced
+				,ROW(null,null)::TilstandsVaerdiRelationAttrType --will be removed in python-layer
+			) :: tilstandRelationType
+				,
+				 ROW (
+					'tilstandsvaerdi'::tilstandRelationKode,
+						virkTilstandsvaerdi1D,
+					null,
+					null,
+					null
+					,1 --NOTICE: Was replaced by import function
+					,ROW(null,null)::TilstandsVaerdiRelationAttrType
+				) :: tilstandRelationType
+			
+				]::TilstandRelationType[]
+
+
+			)::TilstandRegistreringType
+			]::TilstandRegistreringType[]
+		)::TilstandType
+;
+
+raise notice 'expected_tilstand4:%',to_json(expected_tilstand4);	
+
+RETURN NEXT IS(
+	read_Tilstand4,
+	expected_tilstand4
+	,'test update tilstand #4'
+);
+
+/***********************************************/
 
 END;
 $$;
