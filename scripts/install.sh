d3d5a26511792cbce9f375a98e97a18f4e7cac9c
Lars Peter Thomsen <larsp@magenta.dk>
2016-12-16 15:50:54 +0100

    Removed old scripts folder

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-MOXDIR="$DIR/.."
-
-sudo $MOXDIR/apache/set_include.sh -a "$DIR/server-setup/moxscripts.conf"
-sudo a2enmod cgi
-
