41e48b40e6d3adf17ad6d5986596ba69e44c5a51
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2016-12-20 16:16:27 +0100

    auth.sh: fix bash-ism -- bare 'sh' doesn't do 'source'

--- auth.sh
+++ auth.sh
@@ -4,6 +4,6 @@ MOXDIR=$(cd $(dirname $0); pwd)
 
 export PYTHONPATH="$MOXDIR/oio_rest"
 
-source "$MOXDIR/oio_rest/python-env/bin/activate"
+. "$MOXDIR/oio_rest/python-env/bin/activate"
 
 exec python -m oio_rest.auth.tokens "$@"
