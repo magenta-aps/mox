9dd99c54fff31d8cfedd519d05abfcc26d3b7019
Lars Peter Thomsen <larsp@magenta.dk>
2016-12-27 13:46:51 +0100

    Copy upstart config to /etc/init/

--- wso2/install.sh
+++ wso2/install.sh
@@ -5,6 +5,8 @@ DOMAIN=$1
 
 sudo apt-get -y install $(cat "$DIR/SYSTEM_DEPENDENCIES")
 
+sudo cp "$DIR/wso2.conf" "/etc/init/wso2.conf"
+
 sudo NODEBUG=1 $DIR/confWSO2cert.sh "$DOMAIN"
 
 sudo service wso2 restart
