58f355612d14e5557f4bee40518ef3a56122a80f
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-03-15 09:57:47 +0100

    TIA: ensure that errors during agent install propagate

--- agents/MoxTest/install.sh
+++ agents/MoxTest/install.sh
@@ -3,10 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 MOXDIR=$( cd "$DIR/../.." && pwd )
 
-(
-    cd "$DIR"
-    mvn package --quiet -Dmaven.test.skip=true -Dmaven.clean.skip=true
-)
+mvn package --quiet -f "$DIR/pom.xml" -Dmaven.test.skip=true -Dmaven.clean.skip=true
 
 sudo mkdir -p /var/log/mox
 sudo touch /var/log/mox/moxtest.log
