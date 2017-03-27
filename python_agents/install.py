d5b8b639df9ef80731fed4ac885a95bfacbb57c6
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-03-27 17:53:22 +0000

    Log: Use Python installer for Python agents

---
+++ python_agents/install.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import argparse
+import os
+import sys
+from installutils import Service, LogFile, VirtualEnv, expand_template, run
+
+DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+parser = argparse.ArgumentParser(description='Install Python agents')
+args = parser.parse_args()
+
+# ------------------------------------------------------------------------------
+
+
+print 'Installing mox_advis service'
+
+LogFile('/var/log/mox/mox-advis.log', 'moxlog').create()
+VirtualEnv().expand_template('setup/mox_advis.sh.in', mode=755)
+os.chmod('setup/mox_advis.sh', 0755)
+service = Service('setup/mox_advis.sh', user='moxlog',
+                  after=('rabbitmq-server.service'))
+service.install()
+
+print 'Installing mox_elk_log service'
+LogFile('/var/log/mox/mox-elk.log', 'moxlog').create()
+VirtualEnv().expand_template('setup/mox_elk_log.sh.in')
+os.chmod('setup/mox_elk_log.sh', 0755)
+service = Service('setup/mox_elk_log.sh', user='moxlog',
+                  after=('rabbitmq-server.service'))
+service.install()
