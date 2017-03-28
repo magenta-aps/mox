18baec860bae5167adb680fcaef900342ddf1f69
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-03-28 10:45:08 +0200

    Fix incorrect $PWD assumption in python_agents

--- python_agents/install.py
+++ python_agents/install.py
@@ -3,9 +3,7 @@
 import argparse
 import os
 import sys
-from installutils import Service, LogFile, VirtualEnv, expand_template, run
-
-DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+from installutils import DIR, Service, LogFile, VirtualEnv, expand_template, run
 
 parser = argparse.ArgumentParser(description='Install Python agents')
 args = parser.parse_args()
@@ -17,7 +15,7 @@ print 'Installing mox_advis service'
 
 LogFile('/var/log/mox/mox-advis.log', 'moxlog').create()
 VirtualEnv().expand_template('setup/mox_advis.sh.in', mode=755)
-os.chmod('setup/mox_advis.sh', 0755)
+os.chmod(os.path.join(DIR, 'setup/mox_advis.sh'), 0755)
 service = Service('setup/mox_advis.sh', user='moxlog',
                   after=('rabbitmq-server.service'))
 service.install()
@@ -25,7 +23,7 @@ service.install()
 print 'Installing mox_elk_log service'
 LogFile('/var/log/mox/mox-elk.log', 'moxlog').create()
 VirtualEnv().expand_template('setup/mox_elk_log.sh.in')
-os.chmod('setup/mox_elk_log.sh', 0755)
+os.chmod(os.path.join(DIR, 'setup/mox_elk_log.sh'), 0755)
 service = Service('setup/mox_elk_log.sh', user='moxlog',
                   after=('rabbitmq-server.service'))
 service.install()
