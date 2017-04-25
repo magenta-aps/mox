6fe3b46eb625caae3e104ab3fd813774e4b7a265
Heini Leander Ovason <heini.ovason@gmail.com>
2017-04-25 16:10:09 +0200

    Fix installation of python_agents

--- python_agents/install.py
+++ python_agents/install.py
@@ -17,7 +17,7 @@ print 'Installing mox_advis service'
 
 LogFile('/var/log/mox/mox-advis.log', 'moxlog').create()
 VirtualEnv().expand_template('setup/mox_advis.sh.in', mode=755)
-os.chmod('setup/mox_advis.sh', 0755)
+os.chmod(os.path.join(DIR, 'setup/mox_advis.sh'), 0755)
 service = Service('setup/mox_advis.sh', user='moxlog',
                   after=('rabbitmq-server.service'))
 service.install()
@@ -25,7 +25,7 @@ service.install()
 print 'Installing mox_elk_log service'
 LogFile('/var/log/mox/mox-elk.log', 'moxlog').create()
 VirtualEnv().expand_template('setup/mox_elk_log.sh.in')
-os.chmod('setup/mox_elk_log.sh', 0755)
+os.chmod(os.path.join(DIR, 'setup/mox_elk_log.sh'), 0755)
 service = Service('setup/mox_elk_log.sh', user='moxlog',
                   after=('rabbitmq-server.service'))
 service.install()
