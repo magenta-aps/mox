2589651da97c6f3c25886bd0d947a0b189915c6d
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-06-20 16:02:25 +0200

    oio_api: add support for passing arguments to this script

--- oio_rest/oio_api.sh
+++ oio_rest/oio_api.sh
@@ -7,9 +7,7 @@ cd $DIR
 VIRTUALENV=../python-env
 
 if [ "x$USER" != "xmox" ]; then
-	sudo -u mox $VIRTUALENV/bin/oio_api
+	exec sudo -u mox $VIRTUALENV/bin/oio_api "$@"
 else
-	$VIRTUALENV/bin/oio_api
+	exec $VIRTUALENV/bin/oio_api "$@"
 fi
-
-cd -
