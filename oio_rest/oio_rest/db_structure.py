c82c2980625d20a440d0578ff6b89299867f8ac6
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2018-04-23 17:44:45 +0200

    db: fix database generation
    I accidentally broke database generation with my Python 3.x
    preparations. I did this by opting for a layout for states, which no
    longer allowed simple instantiation as a dictionary. Instead, we
    switch to a list of two-tuples.

--- oio_rest/oio_rest/db_structure.py
+++ oio_rest/oio_rest/db_structure.py
@@ -241,10 +241,8 @@ DATABASE_STRUCTURE = {
                 "brugervendtnoegle", "beskrivelse"]
         },
         "tilstande": [
-            "status",
-            ["Inaktiv", "Aktiv"],
-            "publiceret",
-            ["Publiceret", "IkkePubliceret", "Normal"]
+            ["status", ["Inaktiv", "Aktiv"]],
+            ["publiceret", ["Publiceret", "IkkePubliceret", "Normal"]],
         ],
         "relationer_nul_til_en": ["tilstandsobjekt", "tilstandstype"],
         "relationer_nul_til_mange": [
@@ -268,10 +266,8 @@ DATABASE_STRUCTURE = {
             }
         },
         "tilstande": [
-            "status",
-            ["Inaktiv", "Aktiv", "Aflyst"],
-            "publiceret",
-            ["Publiceret", "IkkePubliceret", "Normal"],
+            ["status", ["Inaktiv", "Aktiv", "Aflyst"]],
+            ["publiceret", ["Publiceret", "IkkePubliceret", "Normal"]],
         ],
         "relationer_nul_til_en": ["aktivitetstype", "emne", "foelsomhedklasse",
                                   "ansvarligklasse", "rekvirentklasse",
@@ -298,12 +294,10 @@ DATABASE_STRUCTURE = {
             }
         },
         "tilstande": [
-            "publiceret",
-            ["Publiceret", "IkkePubliceret", "Normal"],
-            "fremdrift",
-            [
+            ["publiceret", ["Publiceret", "IkkePubliceret", "Normal"]],
+            ["fremdrift", [
                 "Uoplyst", "Visiteret", "Disponeret", "Leveret", "Vurderet"
-            ]
+            ]],
         ],
         "relationer_nul_til_en": ["indsatsmodtager", "indsatstype"],
         "relationer_nul_til_mange": [
