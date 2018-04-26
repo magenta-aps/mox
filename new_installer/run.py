883f54e50059559fd1fcc8febcd08c9dbead8a21
Steffen Park <dev@istherepie.com>
2018-04-26 09:08:55 +0200

    18458: Updated run.py to execute the test state

--- new_installer/run.py
+++ new_installer/run.py
@@ -1,18 +1,27 @@
 # -- coding: utf-8 --
+
 import os
+import sys
 import salt.client
 import salt.config
 
-__opts__ = salt.config.minion_config("salt.conf")
 
-# Set root dir:
-__opts__["root_dir"] = "/opt/magenta"
+# Env variables
+base_dir = os.environ.get("BASE_DIR", None)
+installer_dir = os.environ.get("INSTALLER_DIR", None)
+
+
+if not base_dir or not installer_dir:
+    sys.exit(
+        "Installer variables have not been set"
+    )
+
+# Import salt config
+__opts__ = salt.config.minion_config("salt.conf")
 
 # Set file roots (relative to rootdir)
 __opts__['file_roots'] = {
-    "base": [
-       "/opt/magenta/local"
-     ]
+    "base": [installer_dir]
 }
 
 # Minion caller client
@@ -23,5 +32,5 @@ caller = salt.client.Caller(mopts=__opts__)
 testing = caller.cmd("cmd.run", ["whoami"])
 print(testing)
 
-run_state = caller.cmd("state.apply", "common.deps")
+run_state = caller.cmd("state.apply", "tasks.test")
 print(run_state)
