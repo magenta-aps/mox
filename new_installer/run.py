9cad301dc3854272ad245e98d660ec953d168674
Steffen Park <dev@istherepie.com>
2018-04-30 07:33:56 +0200

    18458: Trimmed and updated salt runner to reflect the new file hieraky

--- new_installer/run.py
+++ new_installer/run.py
@@ -6,31 +6,30 @@ import salt.client
 import salt.config
 
 
-# Env variables
-base_dir = os.environ.get("BASE_DIR", None)
-installer_dir = os.environ.get("INSTALLER_DIR", None)
+# Settings
+base_dir = os.getcwd()
 
+# Installer directory
+installer_dir = os.path.dirname(
+    os.path.abspath(__file__)
+)
 
-if not base_dir or not installer_dir:
-    sys.exit(
-        "Installer variables have not been set"
-    )
+# Salt base (Root of salt states)
+salt_base = os.path.join(installer_dir, "base")
 
 # Import salt config
 __opts__ = salt.config.minion_config("salt.conf")
 
 # Set file roots (relative to rootdir)
 __opts__['file_roots'] = {
-    "base": [installer_dir]
+    "base": [
+        salt_base
+    ]
 }
 
 # Minion caller client
 # This used in a masterless configuration
 caller = salt.client.Caller(mopts=__opts__)
 
-# Run command on self
-testing = caller.cmd("cmd.run", ["whoami"])
-print(testing)
-
-run_state = caller.cmd("state.apply", "tasks.test")
+run_state = caller.cmd("state.apply")
 print(run_state)
