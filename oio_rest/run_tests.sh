826a7d94ebae691ad0471bccbe700069e5b92d93
Steffen Park <steffen@magenta.dk>
2018-05-03 15:41:17 +0200

    18459: Added workaround (variables) to test runner

--- oio_rest/run_tests.sh
+++ oio_rest/run_tests.sh
@@ -4,14 +4,30 @@ set -ex
 
 cd $(dirname $0)
 
-# Vars
+# Virtualenv / Python
 TEST_ENV=/tmp/python-test
-PYTHON_EXEC=${TEST_ENV}/bin/python
+PYTHON=${TEST_ENV}/bin/python
 
 # Create virtualenv
 python -m virtualenv --quiet $TEST_ENV
 
+# Temporary workaround
+# These variables are needed to run $ROOT/db/mkdb.sh
+export SUPER_USER="postgres"
+export MOX_DB="mox"
+export MOX_DB_USER="mox"
+export MOX_DB_PASSWORD="mox"
+
+export MOX_AMQP_HOST="localhost"
+export MOX_AMQP_PORT=5672
+export MOX_AMQP_USER="guest"
+export MOX_AMQP_PASS="guest"
+export MOX_AMQP_VHOST="/"
+
+# Install common lib
+$PYTHON -m pip install -e ../lib/common
+
 # Execute tests
-$PYTHON_EXEC -m pip install -r requirements-test.txt
-$PYTHON_EXEC -m flake8 --exit-zero
-$PYTHON_EXEC -m pytest
+$PYTHON -m pip install -r requirements-test.txt
+$PYTHON -m flake8 --exit-zero
+$PYTHON -m pytest
