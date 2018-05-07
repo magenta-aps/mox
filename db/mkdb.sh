df88290b7c9bbbfa9d79f780fe0726b7385d423b
Steffen Park <dev@istherepie.com>
2018-05-07 17:00:22 +0200

    18459: oio_rest_lib module has changed name

--- db/mkdb.sh
+++ db/mkdb.sh
@@ -77,7 +77,7 @@ cat $DIR/funcs/_as_search_match_array.sql
 cat $DIR/funcs/_as_search_ilike_array.sql
 cat $DIR/funcs/_json_object_delete_keys.sql
 
-oiotypes=$($PYTHON -m oio_rest_lib.db_structure)
+oiotypes=$($PYTHON -m oio_common.db_structure)
 
 templates1=( dbtyper-specific tbls-specific _remove_nulls_in_array )
 
