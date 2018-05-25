a0ec2035375368408d2f9fc7192c93fe14174750
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2018-05-25 14:48:09 +0200

    tests: use a single database instance, and TRUNCATE it between runs

--- oio_rest/oio_rest/utils/test_support.py
+++ oio_rest/oio_rest/utils/test_support.py
@@ -24,57 +24,51 @@ import settings
 
 BASE_DIR = os.path.dirname(settings.__file__)
 TOP_DIR = os.path.dirname(BASE_DIR)
+TEMP_DIR = os.path.join(BASE_DIR, 'build', 'tmp')
+
+os.makedirs(TEMP_DIR, exist_ok=True)
+
+psql = testing.postgresql.Postgresql(
+    base_dir=tempfile.mkdtemp(prefix='mox-db-', dir=TEMP_DIR),
+)
+
+atexit.register(psql.stop)
 
 
-def _initdb(psql):
-    dsn = psql.dsn()
+def _initdb():
+    env = {
+        **os.environ,
+        'TESTING': '1',
+        'PYTHON': sys.executable,
+        'MOX_DB': settings.DATABASE,
+        'MOX_DB_USER': settings.DB_USER,
+        'MOX_DB_PASSWORD': settings.DB_PASSWORD,
+    }
 
-    env = os.environ.copy()
-
-    env.update(
-        TESTING='1',
-        PYTHON=sys.executable,
-        MOX_DB=settings.DATABASE,
-        MOX_DB_USER=settings.DB_USER,
-        MOX_DB_PASSWORD=settings.DB_PASSWORD,
-    )
-
-    with psycopg2.connect(**dsn) as conn:
+    with psycopg2.connect(psql.url()) as conn:
         conn.autocommit = True
 
         with conn.cursor() as curs:
-            curs.execute(
-                "CREATE USER {} WITH SUPERUSER PASSWORD %s".format(
-                    settings.DB_USER,
-                ),
-                (
-                    settings.DB_PASSWORD,
-                ),
-            )
+            curs.execute('CREATE USER {} WITH SUPERUSER PASSWORD {!r}'.format(
+                settings.DB_USER, settings.DB_PASSWORD,
+            ))
 
-            curs.execute(
-                "CREATE DATABASE {} WITH OWNER = %s".format(settings.DATABASE),
-                (
-                    settings.DB_USER,
-                ),
-            )
-
-    dsn = dsn.copy()
-    dsn['database'] = settings.DATABASE
-    dsn['user'] = settings.DB_USER
-    dsn['password'] = settings.DB_PASSWORD
+            curs.execute('CREATE DATABASE {} WITH OWNER = {!r}'.format(
+                settings.DATABASE, settings.DB_PASSWORD,
+            ))
 
     mkdb_path = os.path.join(BASE_DIR, '..', 'db', 'mkdb.sh')
+    sql = subprocess.check_output([mkdb_path], env=env)
 
-    with psycopg2.connect(**dsn) as conn, conn.cursor() as curs:
-        curs.execute(subprocess.check_output([mkdb_path], env=env))
+    with psycopg2.connect(psql.url(
+            database=settings.DATABASE,
+            user=settings.DB_USER,
+            password=settings.DB_PASSWORD,
+    )) as conn:
+        conn.autocommit = True
 
-
-psql_factory = testing.postgresql.PostgresqlFactory(
-    cache_initialized_db=True,
-    on_initialized=_initdb
-)
-atexit.register(psql_factory.clear_cache)
+        with conn.cursor() as curs:
+            curs.execute(sql)
 
 
 @pytest.mark.slow
@@ -84,6 +78,7 @@ class TestCaseMixin(object):
     '''
 
     maxDiff = None
+    psql = None
 
     def get_lora_app(self):
         app.app.config['DEBUG'] = False
@@ -93,19 +88,45 @@ class TestCaseMixin(object):
 
         return app.app
 
+    def __reset_db(self):
+        with psycopg2.connect(self.db_url) as conn:
+            conn.autocommit = True
+
+            with conn.cursor() as curs:
+                from oio_common.db_structure import DATABASE_STRUCTURE
+
+                try:
+                    curs.execute("TRUNCATE TABLE {} CASCADE".format(
+                        ', '.join(sorted(DATABASE_STRUCTURE)),
+                    ))
+                except psycopg2.ProgrammingError:
+                    pass
+
     def setUp(self):
         super(TestCaseMixin, self).setUp()
 
-        psql = psql_factory()
-        psql.wait_booting()
+        self.db_url = psql.url(
+            database=settings.DATABASE,
+            user=settings.DB_USER,
+            password=settings.DB_PASSWORD,
+        )
 
-        dsn = psql.dsn()
+        try:
+            with psycopg2.connect(self.db_url) as conn:
+                pass
+        except psycopg2.DatabaseError:
+            _initdb()
 
-        patches = [
+        self.addCleanup(self.__reset_db)
+
+        db_host = psql.dsn()['host']
+        db_port = psql.dsn()['port']
+
+        for p in [
             mock.patch('settings.LOG_AMQP_SERVER', None),
-            mock.patch('settings.DB_HOST', dsn['host'],
+            mock.patch('settings.DB_HOST', db_host,
                        create=True),
-            mock.patch('settings.DB_PORT', dsn['port'],
+            mock.patch('settings.DB_PORT', db_port,
                        create=True),
             mock.patch(
                 'oio_rest.db.pool',
@@ -114,39 +135,51 @@ class TestCaseMixin(object):
                     database=settings.DATABASE,
                     user=settings.DB_USER,
                     password=settings.DB_PASSWORD,
-                    host=dsn['host'],
-                    port=dsn['port'],
+                    host=db_host,
+                    port=db_port,
                 ),
             ),
-        ]
-
-        for p in patches:
+        ]:
             p.start()
             self.addCleanup(p.stop)
 
-        self.addCleanup(psql.stop)
-
-
 
 @click.command()
-@click.option('-p', '--port', type=int, default=5000)
+@click.option('--host', '-h', default='::1',
+              help='The interface to bind to.')
+@click.option('--port', '-p', default=5000,
+              help='The port to bind to.')
+@click.option('use_reloader', '--reload/--no-reload', default=None,
+              help='Enable or disable the reloader.  By default the reloader '
+              'is active if debug is enabled.')
+@click.option('use_debugger', '--debugger/--no-debugger', default=None,
+              help='Enable or disable the debugger.  By default the debugger '
+              'is active if debug is enabled.')
+@click.option('use_reloader', '--eager-loading/--lazy-loader', default=None,
+              help='Enable or disable eager loading.  By default eager '
+              'loading is enabled if the reloader is disabled.')
+@click.option('threaded', '--with-threads/--without-threads', default=True,
+              help='Enable or disable multithreading.')
 def run_with_db(**kwargs):
-    with testing.postgresql.Postgresql(
-        base_dir=tempfile.mkdtemp(prefix='mox'),
-        postgres_args=(
-            '-h localhost -F '
-            '-c logging_collector=off '
-            '-c synchronous_commit=off '
-            '-c fsync=off'
-        ),
-    ) as psql:
+    from oio_rest import db
+
+    with psql:
+        dsn = psql.dsn()
+
         # We take over the process, given that this is a CLI command.
         # Hence, there's no need to restore these variables afterwards
         settings.LOG_AMQP_SERVER = None
-        settings.DB_HOST = psql.dsn()['host']
-        settings.DB_PORT = psql.dsn()['port']
+        settings.DB_HOST = dsn['host']
+        settings.DB_PORT = dsn['port']
 
-        _initdb(psql)
+        db.pool = psycopg2.pool.PersistentConnectionPool(
+            1, 100,
+            database=dsn['database'],
+            user=dsn['user'],
+            password=dsn.get('password'),
+            host=dsn['host'],
+            port=dsn['port'],
+        )
 
         app.app.run(**kwargs)
 
