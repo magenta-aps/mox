a3f6fbd09ab391b0a88c79f0c951a66ecc8ff6a8
Robert Jensen <jensen.robert@gmail.com>
2018-06-20 09:16:52 +0000

    Update mkdb.sh to also allow Jenkins to compile

--- db/initdb.sh
+++ db/initdb.sh
@@ -86,7 +86,7 @@ psql -d $MOX_DB -U $MOX_DB_USER -f ../funcs/_ensure_document_variant_and_del_exi
 psql -d $MOX_DB -U $MOX_DB_USER -f ../funcs/_as_list_dokument_varianter.sql
 
 
-templates2=(  _as_get_prev_registrering _as_create_registrering as_update  as_create_or_import  as_list as_read as_search json-cast-functions  _as_sorted _as_filter_unauth )
+templates2=(  _as_get_prev_registrering _as_create_registrering as_update  as_create_or_import  as_list as_read as_search json-cast-functions _as_sorted _as_filter_unauth )
 
 
 for oiotype in $oiotypes
