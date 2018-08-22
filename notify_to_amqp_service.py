61ae4ca0760add30987df08c44c547c1fc66e930
Robert Jensen <jensen.robert@gmail.com>
2018-08-22 07:09:17 +0000

    Added heartbeat to the amqp-server
    This should allow the external service to verify that the
    relay is actually running

--- notify_to_amqp_service.py
+++ notify_to_amqp_service.py
@@ -1,3 +1,5 @@
+""" Simple class to relay messages from PostgreSQL notifications
+into an AMQP-queue """
 import time
 import json
 import pika
@@ -6,7 +8,7 @@ import psycopg2
 
 class PgnotifyToAmqp(object):
     def __init__(self):
-        # TODO: Fix hard-coded connection params!!!!
+        # TODO: Fix hard-coded connection params
         self.pg_conn = psycopg2.connect(database='mox', user='mox',
                                         password='mox', host='localhost')
         self.pg_cursor = self.pg_conn.cursor()
@@ -15,30 +17,39 @@ class PgnotifyToAmqp(object):
         pika_connection = pika.BlockingConnection(pika_params)
         self.amqp = pika_connection.channel()
         self.amqp.queue_declare(queue='mox.notifications')
+        self.amqp.queue_declare(queue='mox.heartbeat')
+
+    def read_pg_notify(self):
+        """ Read all notifications currently in queue
+        :return: """
+        payloads = []
+        self.pg_cursor.execute("LISTEN mox_notifications;")
+        self.pg_conn.poll()
+        self.pg_conn.commit()
+        while self.pg_conn.notifies:
+            notify = self.pg_conn.notifies.pop(0)
+            payload_dict = json.loads(notify.payload)
+            amqp_payload = {}
+            table = payload_dict['table']
+            objekttype = table[0:table.find('registrering')-1]
+            amqp_payload['beskedtype'] = 'Notificaion'
+            objecktID = payload_dict['data'][objekttype + '_id']
+            amqp_payload['objecktID'] = objecktID
+            amqp_payload['objekttype'] = objekttype
+            lck = payload_dict['data']['registrering']['livscykluskode']
+            amqp_payload['livscykluskode'] = lck
+            payloads.append(amqp_payload)
+        return payloads
 
     def main(self):
         while True:
-            time.sleep(1)
-            print('Sleeping')
-            self.pg_cursor.execute("LISTEN mox_notifications;")
-            self.pg_conn.poll()
-            self.pg_conn.commit()
-            while self.pg_conn.notifies:
-                notify = self.pg_conn.notifies.pop(0)
-                # print(notify.channel)
-                # print(notify.pid)
-                payload_dict = json.loads(notify.payload)
+            time.sleep(2)
+            self.amqp.basic_publish(exchange='',
+                                    routing_key='mox.heartbeat',
+                                    body=json.dumps(time.time()))
 
-                amqp_payload = {}
-                table = payload_dict['table']
-                objekttype = table[0:table.find('registrering')-1]
-                amqp_payload['beskedtype'] = 'Notificaion'
-                objecktID = payload_dict['data'][objekttype + '_id']
-                amqp_payload['objecktID'] = objecktID
-                amqp_payload['objekttype'] = objekttype
-                lck = payload_dict['data']['registrering']['livscykluskode']
-                amqp_payload['livscykluskode'] = lck
-
+            payloads = self.read_pg_notify()
+            for amqp_payload in payloads:
                 self.amqp.basic_publish(exchange='',
                                         routing_key='mox.notifications',
                                         body=json.dumps(amqp_payload))
