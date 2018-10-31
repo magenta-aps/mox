2db327828023fc4d10e06f1be98722f2d513c16c
Carl Bordum Hansen <carl@bordum.dk>
2018-10-31 13:27:14 +0100

    Convert interface_test/test_tilstand.sh to our python test suite

--- oio_rest/tests/test_rest_interface.py
+++ oio_rest/tests/test_rest_interface.py
@@ -457,3 +457,27 @@ class TestIndsats(_TestInterface):
         )
         assert result.status_code == 200
         assert result.get_json()["uuid"] == uuid_
+
+
+class TestTilstand(_TestInterface):
+    def test_tilstand_create(self):
+        result = self.client.post(
+            "tilstand/tilstand",
+            data={
+                "json": open("tests/fixtures/tilstand_opret.json", "rt").read(),
+            },
+        )
+        assert result.status_code == 201
+        uuid_ = result.get_json()["uuid"]
+        assert is_uuid(uuid_)
+
+    def test_tilstand_put(self):
+        uuid_ = str(uuid.uuid4())
+        result = self.client.put(
+            "tilstand/tilstand/%s" % uuid_,
+            data={
+                "json": open("tests/fixtures/tilstand_opret.json", "rt").read(),
+            },
+        )
+        assert result.status_code == 200
+        assert result.get_json()["uuid"] == uuid_
