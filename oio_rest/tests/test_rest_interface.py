7375fe7b65b6cef1c91f03ce34746689d16f6d7e
Carl Bordum Hansen <carl@bordum.dk>
2018-10-31 13:18:09 +0100

    Converted interface_test/test_aktivitet.sh to our python test suite

--- oio_rest/tests/test_rest_interface.py
+++ oio_rest/tests/test_rest_interface.py
@@ -410,3 +410,26 @@ class TestLogHaendelse(_TestInterface):
             )
             assert result_delete.status_code == 202
             assert result_delete.get_json()["uuid"] == uuid_
+
+
+class TestAktivitet(_TestInterface):
+    def test_aktivitet(self):
+        result = self.client.post(
+            "aktivitet/aktivitet",
+            data={
+                "json": open("tests/fixtures/aktivitet_opret.json", "rt").read(),
+            },
+        )
+        assert result.status_code == 201
+        uuid_ = result.get_json()["uuid"]
+        assert is_uuid(uuid_)
+
+        with self.subTest("Update aktivitet"):
+            result_patch = self.client.patch(
+                "aktivitet/aktivitet/%s" % uuid_,
+                data={
+                    "json": open("tests/fixtures/aktivitet_opdater.json", "rt").read(),
+                },
+            )
+            assert result_patch.status_code == 200
+            assert result_patch.get_json()["uuid"] == uuid_
