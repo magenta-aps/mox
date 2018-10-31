a40c7010a4f3689952fbf7b47939afc8f17caeee
Carl Bordum Hansen <carl@bordum.dk>
2018-10-31 11:41:43 +0100

    Converted interface_test/test_itsystem.sh to our python test suite

--- oio_rest/tests/test_rest_interface.py
+++ oio_rest/tests/test_rest_interface.py
@@ -289,3 +289,16 @@ class Test21660PutUpdate(_TestInterface):
         )
         assert result_put.status_code == 200
         assert result_put.get_json()["uuid"] == uuid_
+
+
+class TestItSystem(_TestInterface):
+    def test_it_system(self):
+        result = self.client.post(
+            "organisation/itsystem",
+            data={
+                "json": open("tests/fixtures/itsystem_opret.json", "rt").read(),
+            },
+        )
+        assert result.status_code == 201
+        uuid_ = result.get_json()["uuid"]
+        assert is_uuid(uuid_)
