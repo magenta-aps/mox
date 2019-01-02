0fb462e6dd2b310de1997722872bafb992796f46
Carl Bordum Hansen <carl@bordum.dk>
2019-01-02 15:02:15 +0100

    Remove installer
    install.sh now setups a development environment without all the unneccesary
    jazz such as rabbitmq, nginx, gunicorn and oio_rest service.
    
    This is faster, simpler and produces are more convinient and familiar
    environment to develop in.
    
    Also, we won't have to maintain the installer anymore.

--- install.sh
+++ install.sh
@@ -1,48 +1,83 @@
-#!/bin/bash
-# DOES NOT AUTOMATICALLY INSTALL THE FOLLOWING COMPONENTS:
-# Mox Rest Frontend, Mox Advis, Mox Elk log
+#!/usr/bin/env sh
+# Use install.sh to get a development environment up and running.
+# Don't use this in production.
+
+# This script does not automatically install the following components:
+#   Mox Rest Frontend, Mox Advis, Mox Elk log
+
+# Furthermore, it does not install rabbitmq or notification-service,
+#   based on the assumption that it isn't needed in most development
+#   scenarios. An optional flag for that purpose could be added.
 
 set -e
 
 ## Check if executed as root
-if [ $( whoami ) == "root" ]; then
-    echo "Do not run as root. We'll sudo when necessary"
-exit 1;
+if [ "$( whoami )" == "root" ]; then
+    echo "Do not run as root. We'll sudo when necessary";
+    exit 1;
 fi
 
-## Variables
 
 # Base directory (current)
-BASE_DIR=$(cd $(dirname $0); pwd)
+BASE_DIR=$(cd "$(dirname $0)"; pwd)
 
-# Setup directory
-INSTALLER_DIR=${BASE_DIR}/installer
 
-# Setup executable
-CONFIGURE=${INSTALLER_DIR}/configure.py
-INSTALLER=${INSTALLER_DIR}/install_application.py
-
-# Setup requirements
-REQUIREMENTS=${INSTALLER_DIR}/install_requirements.txt
-
-# Path to (python) virtual environment
-export VIRTUALENV=${BASE_DIR}/python-env
-
-# Path to python executable
-export PYTHON_EXEC=${VIRTUALENV}/bin/python
-
-## Install system dependencies
+echo "# Installing system dependencies"
+echo "## Update system registry"
 sudo apt-get -qq update
-sudo apt-get -qy install python3 python3-venv python3-dev gcc
+echo "## Install Python with venv"
+sudo apt-get -qy install python3 python3-venv python3-pip python3-dev gcc
+sudo apt-get -qy install build-essential libxmlsec1-dev
+echo "## Install Postgresql"
+sudo apt-get -qy install postgresql postgresql-common postgresql-client \
+    postgresql-server-dev-all postgresql-contrib pgtap
 
-## Create virtual environment
-/usr/bin/env python3 -m venv ${VIRTUALENV}
 
-## Install python (installer) dependencies
-$PYTHON_EXEC -m pip install -r $REQUIREMENTS
+echo "# Create directories"
+echo "## Create upload directory"
+sudo mkdir /var/mox
+sudo chmod 755 /var/mox
 
-# Collect info and set information (grains)
-$PYTHON_EXEC $CONFIGURE
+echo "## Create log directory"
+sudo mkdir /var/log/mox
+sudo chmod 755 /var/log/mox
 
-## Run installer (as SUPERUSER)
-sudo $PYTHON_EXEC $INSTALLER
+echo "## Create audit log directory"
+sudo touch /var/log/mox/audit.log
+sudo chmod 644 /var/log/mox/audit.log
+
+
+echo "# Allow access to postgresql over TCP"
+echo "## Update pg_hba.conf"
+echo "local all all md5" | sudo tee -a /etc/postgresql/9.5/main/pg_hba.conf > /dev/null
+echo "## Apply changes"
+sudo service postgresql restart
+sudo service postgresql status  # make sure that postgresql is running
+
+
+echo "# Create virtual environment"
+/usr/bin/env python3 -m venv $BASE_DIR/python-env
+
+
+echo "# Install requirements"
+/$BASE_DIR/python-env/bin/pip install -r $BASE_DIR/oio_rest/requirements.txt
+
+
+echo "# Install oio_rest package"
+/$BASE_DIR/python-env/bin/pip install -e $BASE_DIR/oio_rest
+
+
+echo "# Initialize database"
+$BASE_DIR/db/initdb.sh
+
+
+echo -e "\n# Installation completed successfully."
+echo "To activate the oio_rest virtual environment:
+    '. $BASE_DIR/python-env/bin/activate'"
+echo "When activated, you can start oio_rest:
+    'python -m flask run'"
+echo "When activated, you can run the test suite:
+    'pip install -r $BASE_DIR/oio_rest/requirements-test.txt' once,
+    'pytest' from within $BASE_DIR/oio_rest."
+echo "To deactivate the oio_rest virtual environment:
+    'deactivate'"
