19a486a09b95ce158f378605607052f16ee1df79
Carl Bordum Hansen <carl@bordum.dk>
2019-01-07 10:03:28 +0000

    printf > echo
    echo is nonstandard in most shells and posix recommends against it

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ set -e
 
 ## Check if executed as root
 if [ "$( whoami )" == "root" ]; then
-    echo "Do not run as root. We'll sudo when necessary";
+    printf "Do not run as root. We'll sudo when necessary\n";
     exit 1;
 fi
 
@@ -22,64 +22,65 @@ fi
 BASE_DIR=$(cd "$(dirname $0)"; pwd)
 
 
-echo "# Installing system dependencies"
-echo "## Update system registry"
+printf "# Installing system dependencies\n"
+printf "## Update system registry\n"
 sudo apt-get -qq update
-echo "## Install Python with venv"
+printf "## Install Python with venv\n"
 sudo apt-get -qy install python3 python3-venv python3-pip python3-dev gcc
 sudo apt-get -qy install build-essential libxmlsec1-dev
-echo "## Install Postgresql"
+printf "## Install Postgresql\n"
 sudo apt-get -qy install postgresql postgresql-common postgresql-client \
     postgresql-server-dev-all postgresql-contrib pgtap
-echo "## Install AMQP"
+printf "## Install AMQP\n"
 sudo apt-get -qy install rabbitmq-server
 
 
-echo "# Create directories"
-echo "## Create upload directory"
+printf "# Create directories\n"
+printf "## Create upload directory\n"
 sudo mkdir /var/mox
 sudo chmod 755 /var/mox
 
-echo "## Create log directory"
+printf "## Create log directory\n"
 sudo mkdir /var/log/mox
 sudo chmod 755 /var/log/mox
 
-echo "## Create audit log directory"
+printf "## Create audit log directory\n"
 sudo touch /var/log/mox/audit.log
 sudo chmod 644 /var/log/mox/audit.log
 
 
-echo "# Allow access to postgresql over TCP"
-echo "## Update pg_hba.conf"
-echo "local all all md5" | sudo tee -a /etc/postgresql/9.5/main/pg_hba.conf > /dev/null
-echo "## Apply changes"
+printf "# Allow access to postgresql over TCP\n"
+printf "## Update pg_hba.conf\n"
+printf "local all all md5" | sudo tee -a /etc/postgresql/9.5/main/pg_hba.conf > /dev/null
+printf "## Apply changes\n"
 sudo service postgresql restart
 sudo service postgresql status  # make sure that postgresql is running
 
 
-echo "# Create virtual environment"
+printf "# Create virtual environment\n"
 /usr/bin/env python3 -m venv $BASE_DIR/python-env
 
 
-echo "# Install requirements"
+printf "# Install requirements\n"
 /$BASE_DIR/python-env/bin/pip install -r $BASE_DIR/oio_rest/requirements.txt
 
 
-echo "# Install oio_rest package"
+printf "# Install oio_rest package\n"
 /$BASE_DIR/python-env/bin/pip install -e $BASE_DIR/oio_rest
 
 
-echo "# Initialize database"
+printf "# Initialize database\n"
 $BASE_DIR/db/initdb.sh
 
 
-echo -e "\n# Installation completed successfully."
-echo "To activate the oio_rest virtual environment:
-    '. $BASE_DIR/python-env/bin/activate'"
-echo "When activated, you can start oio_rest:
-    'python -m flask run'"
-echo "When activated, you can run the test suite:
+printf "\n# Installation completed successfully.\n"
+printf "To activate the oio_rest virtual environment:
+    '. $BASE_DIR/python-env/bin/activate'\n"
+printf "When activated, you can start oio_rest:
+    'python -m flask run'\n"
+printf "When activated, you can run the test suite:
     'pip install -r $BASE_DIR/oio_rest/requirements-test.txt' once,
-    'pytest' from within $BASE_DIR/oio_rest."
-echo "To deactivate the oio_rest virtual environment:
-    'deactivate'"
+    'pytest' from within $BASE_DIR/oio_rest.\n"
+printf "To deactivate the oio_rest virtual environment:
+    'deactivate'\n"
+
