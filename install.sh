6db4d7ee36f495cf24fa6f6c4cf5e984a34f881c
Carl Bordum Hansen <carl@bordum.dk>
2019-01-07 13:12:43 +0000

    Implement --full.
    install.sh takes a --full flag which makes a user, installs oio_rest as a
    service, runs nginx and unicorn. rabbitmq is installed either way.

--- install.sh
+++ install.sh
@@ -9,14 +9,26 @@
 #   based on the assumption that it isn't needed in most development
 #   scenarios. An optional flag for that purpose could be added.
 
+# To get a more complete environment, use `--full`. This will install oio_rest
+#   as a service and run nginx and gunicorn all from a seperate user "mox".
+
 set -e
 
+
 ## Check if executed as root
-if [ "$( whoami )" == "root" ]; then
+if [ "$( whoami )" = "root" ]; then
     printf "Do not run as root. We'll sudo when necessary\n";
     exit 1;
 fi
 
+USER=$(whoami)
+if [ "$1" = "--full" ]; then
+    FULL=1
+    USER="mox"
+    sudo useradd --system --password mox -U -s /usr/sbin/nologin mox
+else
+    FULL=0
+fi
 
 # Base directory (current)
 BASE_DIR=$(cd "$(dirname $0)"; pwd)
@@ -33,25 +45,29 @@ sudo apt-get -qy install postgresql postgresql-common postgresql-client \
     postgresql-server-dev-all postgresql-contrib pgtap
 printf "## Install AMQP\n"
 sudo apt-get -qy install rabbitmq-server
+if [ $FULL -eq 1 ]; then
+    printf "## Install nginx\n"
+    sudo apt-get -qy install nginx
+fi
 
 
 printf "# Create directories\n"
 printf "## Create upload directory\n"
 sudo mkdir /var/mox
-sudo chmod 755 /var/mox
+sudo chown "$USER" /var/mox
 
 printf "## Create log directory\n"
 sudo mkdir /var/log/mox
-sudo chmod 755 /var/log/mox
+sudo chown "$USER" /var/log/mox
 
 printf "## Create audit log directory\n"
 sudo touch /var/log/mox/audit.log
-sudo chmod 644 /var/log/mox/audit.log
+sudo chown "$USER" /var/log/mox/audit.log
 
 
 printf "# Allow access to postgresql over TCP\n"
 printf "## Update pg_hba.conf\n"
-printf "local all all md5" | sudo tee -a /etc/postgresql/9.5/main/pg_hba.conf > /dev/null
+printf "local all all md5\n" | sudo tee -a /etc/postgresql/9.5/main/pg_hba.conf > /dev/null
 printf "## Apply changes\n"
 sudo service postgresql restart
 sudo service postgresql status  # make sure that postgresql is running
@@ -68,19 +84,93 @@ printf "# Install requirements\n"
 printf "# Install oio_rest package\n"
 /$BASE_DIR/python-env/bin/pip install -e $BASE_DIR/oio_rest
 
+if [ $FULL -eq 1 ]; then
+    printf "# Install gunicorn\n"
+    /$BASE_DIR/python-env/bin/pip install gunicorn
+fi
+
 
 printf "# Initialize database\n"
 $BASE_DIR/db/initdb.sh
 
 
-printf "\n# Installation completed successfully.\n"
-printf "To activate the oio_rest virtual environment:
-    '. $BASE_DIR/python-env/bin/activate'\n"
-printf "When activated, you can start oio_rest:
-    'python -m flask run'\n"
-printf "When activated, you can run the test suite:
-    'pip install -r $BASE_DIR/oio_rest/requirements-test.txt' once,
-    'pytest' from within $BASE_DIR/oio_rest.\n"
-printf "To deactivate the oio_rest virtual environment:
-    'deactivate'\n"
+if [ $FULL -eq 1 ]; then
+    printf "# Make oio_rest.service\n"
+    printf "
+[Unit]
+Description=\"MOX OIO Rest Interface\"
 
+[Service]
+Type=simple
+Restart=on-failure
+
+User=mox
+Group=mox
+
+WorkingDirectory=$BASE_DIR
+ExecStart=$BASE_DIR/python-env/bin/gunicorn \
+    --bind 127.0.0.1:8080 \
+    --log-syslog-prefix mox \
+    --workers 4 \
+    --access-logfile /var/log/mox/oio_access.log \
+    --error-logfile /var/log/mox/oio_error.log \
+    oio_rest.app:app
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/oio_rest.service > /dev/null
+    sudo touch /var/log/mox/oio_access.log
+    sudo chown "$USER" /var/log/mox/oio_access.log
+    sudo touch /var/log/mox/oio_error.log
+    sudo chown "$USER" /var/log/mox/oio_error.log
+    printf "
+server {
+    listen 80;
+
+    # Virtual server name
+    server_name _;
+
+    # Do NOT send server details
+    server_tokens off;
+
+    # Path to static content
+    root /var/www/html;
+
+    # Proxy
+    location / {
+        # Proxy_pass configuration
+        proxy_set_header Connection '';
+        chunked_transfer_encoding off;
+        proxy_buffering off;
+        proxy_cache off;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \$http_host;
+        proxy_set_header X-NginX-Proxy true;
+        proxy_http_version 1.1;
+        proxy_max_temp_file_size 0;
+        proxy_pass http://localhost:8080/;
+        proxy_redirect off;
+        proxy_read_timeout 240s;
+    }
+}" | sudo tee /etc/nginx/sites-available/mox > /dev/null
+    sudo ln -s /etc/nginx/sites-available/mox /etc/nginx/sites-enabled/mox
+    sudo rm /etc/nginx/sites-enabled/default
+fi
+
+
+printf "\n# Installation completed successfully.\n"
+if [ $FULL -eq 0 ]; then
+    printf "To activate the oio_rest virtual environment:
+        '. $BASE_DIR/python-env/bin/activate'\n"
+    printf "When activated, you can start oio_rest:
+        'python -m flask run'\n"
+    printf "When activated, you can run the test suite:
+        'pip install -r $BASE_DIR/oio_rest/requirements-test.txt' once,
+        'pytest' from within $BASE_DIR/oio_rest.\n"
+    printf "To deactivate the oio_rest virtual environment:
+        'deactivate'\n"
+else
+    sudo service oio_rest restart
+    sudo service nginx restart
+    printf "oio_rest running on localhost\n"
+fi
