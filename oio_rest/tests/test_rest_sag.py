7f1c0547947b6ee3608c1427bfd7a0ab15bb021b
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2019-02-19 17:44:23 +0100

    test_rest_sag: split out to individual tests

--- oio_rest/tests/test_rest_sag.py
+++ oio_rest/tests/test_rest_sag.py
@@ -14,119 +14,130 @@ from tests import util
 
 
 class TestSag(util.TestCase):
+    def setUp(self):
+        super().setUp()
+
+        result = self.client.post(
+            "/sag/sag",
+            json=util.get_fixture("sag_opret.json"),
+        ).get_json()
+        self.assertTrue(is_uuid(result["uuid"]))
+        self.uuid = result["uuid"]
+
+    def test_sag_1(self):
+        "Search for andrebehandlere relation"
+        search1 = self.client.get(
+            "sag/sag",
+            query_string={
+                "andrebehandlere": "ef2713ee-1a38-4c23-8fcb-3c4331262194",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search1.status_code, 200)
+        self.assertEqual(search1.get_json()["results"][0][0], self.uuid)
+
     @unittest.expectedFailure
-    def test_sag(self):
-        with self.subTest("Create sag"):
-            result = self.client.post(
-                "/sag/sag",
-                data={
-                    "json": open("tests/fixtures/sag_opret.json", "rt").read(),
-                }
-            ).get_json()
-            self.assertTrue(is_uuid(result["uuid"]))
-            uuid_ = result["uuid"]
+    def test_sag_2(self):
+        "Search for journalpostkode relation"
+        # unsupported argument: journalpostkode
+        search2 = self.client.get(
+            "sag/sag",
+            query_string={
+                "journalpostkode": "journalnotat",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search2.status_code, 200)
+        self.assertEqual(search2.get_json()["results"][0][0], self.uuid)
 
-        with self.subTest("Search on case andrebehandlere relation"):
-            search1 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "andrebehandlere": "ef2713ee-1a38-4c23-8fcb-3c4331262194",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search1.status_code, 200)
-            self.assertEqual(search1.get_json()["results"][0][0], uuid_)
+    def test_sag_3(self):
+        "Search for wrong journalpostkode relation"
+        # unsupported argument: journalpostkode
+        search3 = self.client.get(
+            "sag/sag",
+            query_string={
+                "journalpostkode": "tilakteretdokument",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search3.status_code, 400)
 
-        with self.subTest("Search on case journalpostkode relation"):
-            # unsupported argument: journalpostkode
-            search2 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "journalpostkode": "journalnotat",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search2.status_code, 200)
-            self.assertEqual(search2.get_json()["results"][0][0], uuid_)
+    @unittest.expectedFailure
+    def test_sag_4(self):
+        "Search for journalnotat.titel relation"
+        # unsupported argument: journalnotat.titel
+        search4 = self.client.get(
+            "sag/sag",
+            query_string={
+                "journalnotat.titel": "Kommentarer",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search4.status_code, 200)
+        self.assertEqual(search4.get_json()["results"][0][0], self.uuid)
 
-        with self.subTest("Search on case wrong journalpostkode relation"):
-            # unsupported argument: journalpostkode
-            search3 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "journalpostkode": "tilakteretdokument",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search3.status_code, 400)
+    def test_sag_5(self):
+        "Search for wrong journalnotat.titel relation"
+        # unsupported argument: journalnotat.titel
+        search5 = self.client.get(
+            "sag/sag",
+            query_string={
+                "journalnotat.titel": "Wrong",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search5.status_code, 400)
 
-        with self.subTest("Search on case journalnotat.titel relation"):
-            # unsupported argument: journalnotat.titel
-            search4 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "journalnotat.titel": "Kommentarer",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search4.status_code, 200)
-            self.assertEqual(search4.get_json()["results"][0][0], uuid_)
+    @unittest.expectedFailure
+    def test_sag_6(self):
+        "Search for journaldokument.dokumenttitel relation"
+        # unsupported argument: journaldokument.dokumenttitel
+        search6 = self.client.get(
+            "sag/sag",
+            query_string={
+                "journaldokument.dokumenttitel": "Rapport",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search6.status_code, 200)
+        self.assertEqual(search6.get_json()["results"][0][0], self.uuid)
 
-        with self.subTest("Search on case wrong journalnotat.titel relation"):
-            # unsupported argument: journalnotat.titel
-            search5 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "journalnotat.titel": "Wrong",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search5.status_code, 400)
+    def test_sag_7(self):
+        "Search for wrong journaldokument.dokumenttitel relation"
+        # unsupported argument: journaldokument.dokumenttitel
+        search7 = self.client.get(
+            "sag/sag",
+            query_string={
+                "journaldokument.dokumenttitel": "Wrong",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search7.status_code, 400)
 
-        with self.subTest("Search on case journaldokument.dokumenttitel relation"):
-            # unsupported argument: journaldokument.dokumenttitel
-            search6 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "journaldokument.dokumenttitel": "Rapport",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search6.status_code, 200)
-            self.assertEqual(search6.get_json()["results"][0][0], uuid_)
+    @unittest.expectedFailure
+    def test_sag_8(self):
+        "Search for journaldokument.offentligtundtaget.alternativtitel relation"
+        # unsupported argument:
+        # journaldokument.offenligtundtaget.alternativtitel
+        search8 = self.client.get(
+            "sag/sag",
+            query_string={
+                "journaldokument.offentligtundtaget.alternativtitel": "Fortroligt",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search8.status_code, 200)
+        self.assertEqual(search8.get_json()["results"][0][0], self.uuid)
 
-        with self.subTest("Search on case wrong journaldokument.dokumenttitel relation"):
-            # unsupported argument: journaldokument.dokumenttitel
-            search7 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "journaldokument.dokumenttitel": "Wrong",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search7.status_code, 400)
-
-        with self.subTest("Search on case journaldokument.offentligtundtaget.alternativtitel relation"):
-            # unsupported argument:
-            # journaldokument.offenligtundtaget.alternativtitel
-            search8 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "journaldokument.offentligtundtaget.alternativtitel": "Fortroligt",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search8.status_code, 200)
-            self.assertEqual(search8.get_json()["results"][0][0], uuid_)
-
-        with self.subTest("Search on case wrong journaldokument.offentligtundtaget.alternativtitel relation"):
-            # unsupported argument:
-            # journaldokument.offenligtundtaget.alternativtitel
-            search9 = self.client.get(
-                "sag/sag",
-                query_string={
-                    "journaldokument.offentligtundtaget.alternativtitel": "Wrong",
-                    "uuid": uuid_,
-                },
-            )
-            self.assertEqual(search9.status_code, 400)
+    def test_sag_9(self):
+        "Search for wrong journaldokument.offentligtundtaget.alternativtitel relation"
+        # unsupported argument:
+        # journaldokument.offenligtundtaget.alternativtitel
+        search9 = self.client.get(
+            "sag/sag",
+            query_string={
+                "journaldokument.offentligtundtaget.alternativtitel": "Wrong",
+                "uuid": self.uuid,
+            },
+        )
+        self.assertEqual(search9.status_code, 400)
