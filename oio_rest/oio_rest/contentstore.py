6e4eb12e06a6b26d5d256f0e0e599cb08ebbb739
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2019-02-26 18:03:18 +0100

    style: get rid of all "encoding: utf-8" comments -- the default in py3k

--- oio_rest/oio_rest/contentstore.py
+++ oio_rest/oio_rest/contentstore.py
@@ -5,9 +5,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-# encoding: utf-8
-
 import os
 from urllib.parse import urlparse
 import uuid
--- oio_rest/oio_rest/oio_rest.py
+++ oio_rest/oio_rest/oio_rest.py
@@ -5,9 +5,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-# encoding: utf-8
 """Superclasses for OIO objects and object hierarchies."""
+
 import json
 import datetime
 
