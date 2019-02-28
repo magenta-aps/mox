4db6e07dd144ee1708f63a5a4f38fe3e24c3f06b
Mathias Dannesbo <md@magenta.dk>
2019-02-28 18:06:20 +0100

    Update to new settings in oio_rest

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -c '\l'; do
+until PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -c '\l'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
 done
 
 >&2 echo "Postgres is up - continuing"
 
-if ! PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -t -c '\dn actual_state' | grep -q .; then
+if ! PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -t -c '\dn actual_state' | grep -q .; then
     echo "Initializing database"
-    PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -v ON_ERROR_STOP=1 <<EOF
+    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -v ON_ERROR_STOP=1 <<EOF
 CREATE SCHEMA actual_state AUTHORIZATION $POSTGRES_USER;
 ALTER DATABASE mox SET search_path TO actual_state, public;
 ALTER DATABASE mox SET DATESTYLE to 'ISO, YMD';
@@ -19,7 +19,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA actual_state;
 CREATE EXTENSION IF NOT EXISTS "btree_gist" WITH SCHEMA actual_state;
 CREATE EXTENSION IF NOT EXISTS "pg_trgm" WITH SCHEMA actual_state;
 EOF
-    python3 -m oio_rest sql | PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d mox -v ON_ERROR_STOP=1
+    python3 -m oio_rest sql | PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d mox -v ON_ERROR_STOP=1
 fi
 
 exec "$@"
