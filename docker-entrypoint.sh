efcc2677016df207821c8ca658a7f0651e0ddb1f
Carl Bordum Hansen <carl@bordum.dk>
2019-05-28 13:34:21 +0200

    Remove env vars from ``docker-entrypoint.sh``
    They are set by ``oio_rest/settings.py``.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -e
 
-# This docker image only supports DB_* and DATABASE used by
-# ``oio_rest/settings.py``. We use the same defaults.
-export DB_HOST=${DB_HOST-localhost}
-export DB_PORT=${DB_PORT-5432}
-export DB_USER=${DB_USER-mox}
-export DB_PASSWORD=${DB_PASSWORD-mox}
-export DATABASE=${DATABASE-mox}
-
 python3 -m oio_rest initdb
 
 exec "$@"
