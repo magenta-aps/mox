a6af4b93700f60eeee6facf749dc5ab18380b955
Carl Bordum Hansen <carl@bordum.dk>
2019-06-13 16:54:15 +0200

    create-extensions as shell script
    Instead of using raw sql. We need this because the super user (through
    docker entrypoint) has to be connected to the mox database when creating
    the extensions.

--- docker/create-extensions.sql
+++
@@ -1,3 +0,0 @@
-create extension if not exists "uuid-ossp" with schema actual_state;
-create extension if not exists "btree_gist" with schema actual_state;
-create extension if not exists "pg_trgm" with schema actual_state;
