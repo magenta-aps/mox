60d946c0acf944a5a71c4910abea99baf59759a9
Carl Bordum Hansen <carl@bordum.dk>
2019-06-27 10:31:11 +0200

    Config db name with environment variable

--- docker/postgres-initdb.d/20-create-extensions.sh
+++ docker/postgres-initdb.d/20-create-extensions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 -d mox <<-EOSQL
+psql -v ON_ERROR_STOP=1 -d $DB_NAME <<-EOSQL
     create extension if not exists "uuid-ossp" with schema actual_state;
     create extension if not exists "btree_gist" with schema actual_state;
     create extension if not exists "pg_trgm" with schema actual_state;
