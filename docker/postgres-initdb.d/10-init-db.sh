aeb65128018c4a239fb56d65acbe390a467be4ae
Carl Bordum Hansen <carl@bordum.dk>
2019-06-27 10:18:41 +0200

    Remove duplicate sql

--- docker/postgres-initdb.d/10-init-db.sh
+++ docker/postgres-initdb.d/10-init-db.sh
@@ -5,19 +5,12 @@ psql -v ON_ERROR_STOP=1 <<-EOSQL
     create user $DB_USER with encrypted password '$DB_PASSWORD';
     create database mox;
     grant all privileges on database mox to $DB_USER;
-    create schema actual_state authorization $DB_USER;
     alter database mox set search_path to actual_state, public;
     alter database mox set datestyle to 'ISO, YMD';
     alter database mox set intervalstyle to 'sql_standard';
-EOSQL
-
-psql -v ON_ERROR_STOP=1 -d mox <<-EOSQL
+    \connect mox
     create schema actual_state authorization $DB_USER;
-    alter database mox set search_path to actual_state, public;
-    alter database mox set datestyle to 'ISO, YMD';
-    alter database mox set intervalstyle to 'sql_standard';
 EOSQL
-
 
 # we can connect without password because ``trust`` authentication for Unix
 # sockets is enabled inside the container.
