415b59fd7af7cbcb3138afbf24315938dd8515d7
Carl Bordum Hansen <carl@bordum.dk>
2019-09-03 14:46:27 +0200

    refactor db config into 15-conf.sh

--- docker/postgres-initdb.d/10-init-db.sh
+++ docker/postgres-initdb.d/10-init-db.sh
@@ -9,12 +9,6 @@ psql -v ON_ERROR_STOP=1 <<-EOSQL
     create user $DB_USER with encrypted password '$DB_PASSWORD';
     create database $DB_NAME;
     grant all privileges on database $DB_NAME to $DB_USER;
-    alter database $DB_NAME set search_path to actual_state, public;
-    alter database $DB_NAME set datestyle to 'ISO, YMD';
-    alter database $DB_NAME set intervalstyle to 'sql_standard';
-    -- These needs to be off. See #21273 and #23145.
-    alter database $DB_NAME set enable_hashagg to off;
-    alter database $DB_NAME set enable_sort to off;
     \connect $DB_NAME
     create schema actual_state authorization $DB_USER;
 EOSQL
