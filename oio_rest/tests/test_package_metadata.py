b31c882c9641d2b4c569bbbc686a5518fec564ae
Christoffer Moesgaard <christoffer@magenta.dk>
2019-12-04 11:21:59 +0100

    Fix version test

--- oio_rest/tests/test_package_metadata.py
+++ oio_rest/tests/test_package_metadata.py
@@ -10,13 +10,14 @@ import os
 import re
 import unittest
 
+from oio_rest import __version__
+
 from . import util
 
 
 class VersionTest(unittest.TestCase):
     def test_versions(self):
-        with open(os.path.join(util.TOP_DIR, 'VERSION')) as fp:
-            main_version = fp.read().strip()
+        main_version = __version__
 
         with open(os.path.join(util.TOP_DIR, 'NEWS.rst')) as fp:
             all_versions = re.findall(r'^Version ([^,]*),', fp.read(),
