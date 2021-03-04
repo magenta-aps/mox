87decc90862ad2be9114c97c96c783ad50ad8aa0
Emil Madsen <sovende@gmail.com>
2021-03-04 09:32:07 +0100

    Fixup document download

--- oio_rest/oio_rest/dokument.py
+++ oio_rest/oio_rest/dokument.py
@@ -1,7 +1,9 @@
 # SPDX-FileCopyrightText: 2015-2020 Magenta ApS
 # SPDX-License-Identifier: MPL-2.0
 
-import flask
+import pydantic
+from fastapi import Request
+from fastapi.responses import FileResponse
 
 from . import db
 from .authentication import requires_auth
@@ -9,6 +11,14 @@ from .contentstore import content_store
 from .db import db_helpers
 
 from .oio_base import OIORestObject, OIOStandardHierarchy
+
+uuid_regex = (
+    "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}"
+    "-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}"
+)
+date_path_regex = "^\\d{4}/\\d{2}/\\d{2}/\\d{2}/\\d{2}/" + uuid_regex + ".bin$"
+
+ContentPath = pydantic.constr(regex=date_path_regex)
 
 
 class Dokument(OIORestObject):
@@ -18,10 +28,11 @@ class Dokument(OIORestObject):
 
     @classmethod
     @requires_auth
-    def download_content(cls, content_path):
+    def download_content(cls, content_path: ContentPath, request: Request):
         """
         Download a content file, given a content path.
         """
+        content_path = str(content_path)
         content_url = "store:%s" % content_path
 
         # Get the document UUID, and the content's mimetype associated with
@@ -36,7 +47,7 @@ class Dokument(OIORestObject):
         filename = content_store.get_filename_for_url(content_url)
 
         # Send the file efficiently, if possible
-        return flask.send_file(filename, mimetype=mimetype)
+        return FileResponse(filename, media_type=mimetype)
 
     @classmethod
     def create_api(cls, hierarchy):
@@ -44,16 +55,7 @@ class Dokument(OIORestObject):
         rest_router = super(Dokument, cls).create_api(hierarchy)
         hierarchy = hierarchy.lower()
         class_url = "/{0}/{1}".format(hierarchy, cls.__name__.lower())
-        # TODO: Fix url to FASTAPI
-        uuid_regex = (
-            "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}"
-            "-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}"
-        )
-        date_path_regex = "\\d{4}/\\d{2}/\\d{2}/\\d{2}/\\d{2}/" + uuid_regex + ".bin"
-        download_content_url = '{0}/<regex("{1}"):content_path>'.format(
-            class_url, date_path_regex
-        )
-
+        download_content_url = class_url + '/{content_path}'
         rest_router.get(
             download_content_url, name="_".join([cls.__name__, "download_content"])
         )(cls.download_content)
