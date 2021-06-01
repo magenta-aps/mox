dc275cf5eb7365a60533bb33ba2a8f008ec30913
Christoffer Moesgaard <christoffer@magenta.dk>
2021-06-01 10:13:48 +0200

    [#43364] Add new configuration file

---
+++ oio_rest/oio_rest/config.py
@@ -0,0 +1,57 @@
+# SPDX-FileCopyrightText: 2021- Magenta ApS
+# SPDX-License-Identifier: MPL-2.0
+
+from functools import lru_cache
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    These settings can be overwritten by environment variables
+    The environement variable name is the upper-cased version of the variable name below
+    E.g. DB_NAME == db_name
+    """
+
+    db_name: str = "mox"
+    db_user: str = "mox"
+    db_password: str
+    db_host: str = "mox-db"
+    db_port: str = "5432"
+
+    # Prefix for all relative URLs. A value of "/MyOIO" will result in API
+    # endpoints such ``http://example.com/MyOIO/organisation/organisationenhed``.
+    base_url: str = ""
+
+    # Path to DB_STRUCTURE extensions
+    db_extensions_path: Optional[str]
+
+    # If enabled, expose /testing/db-* endpoint for setup, reset and teardown of a
+    # test database. Useful for integration tests from other components such as MO.
+    # Does not work when running multi-threaded.
+    testing_api: bool = False
+
+    # If enabled, exposes /db/truncate endpoint, for truncating the current
+    # database.
+    truncate_api: bool = False
+
+    # The log level for the Python application
+    lora_log_level: str = "WARNING"
+
+    # This is where file uploads are stored. It must be readable and writable by
+    # the mox user, running the REST API server. This is used in the Dokument
+    # hierarchy.
+    file_upload_folder: Optional[str]
+
+    # If enabled, uses alternative search implementation
+    quick_search: bool = True
+
+    # Whether authorization is enabled.
+    # If not, the restrictions module is not called.
+    enable_restrictions: bool = False
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
